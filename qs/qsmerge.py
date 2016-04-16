3dbbdcef2635df21decacc34d8956efa9e9a1c2e
John Sturdy <jcg.sturdy@gmail.com>
2016-04-16 20:41:42 +0100

    Allow different file types, and start filling in weight completion
    It can now deduce the file type (or be told it), and can fill in some
    columns of weight data from other columns.

--- qs/qsmerge.py
+++ qs/qsmerge.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Time-stamp: <2016-04-16 19:57:24 jcgs>
+# Time-stamp: <2016-04-16 20:31:06 jcgs>
 
 # Program to merge my Quantified Self files.
 
@@ -14,12 +14,37 @@ def weight_tracker_parser(raw):
     else:
         return { 'Date': iso8601_date(raw[1]), 'Kg': raw[0]}
 
-row_parsers = {
-    "Weight Tracker": weight_tracker_parser
+def weight_tracker_complete_row(row):
+    if 'Lbs total' not in row or row['Lbs total'] == '':
+        if 'Kg' in row and row['Kg'] != '':
+            row['Lbs total'] = float(row['Kg']) * 2.20462
+        elif 'Stone' in row and row['Stone'] != '' and 'Lbs' in row and row['Lbs'] != '':
+            row['Lbs total'] = float(row['Stone']) * 14 + float(row['Lbs'])
+        else:
+            print "Could not derive 'Lbs total' for row", row
+    if 'Kg' not in row or row['Kg'] == '':
+        if 'Lbs total' in row and row['Lbs total'] != '':
+            row['Kg'] = float(row['Lbs total']) / 2.20462
+        else:
+            print "Could not derive 'Kg' for row", row
+    if 'Lbs' not in row or row['Lbs'] == '':
+        if 'Lbs total' in row and row['Lbs total'] != '':
+            row['Lbs'] = int(row['Lbs total']) % 14
+    if 'Stone' not in row or row['Stone'] == '':
+        if 'Lbs total' in row and row['Lbs total'] != '':
+            row['Stone'] = int(row['Lbs total']) / 14
+
+file_type_handlers = {
+    'weight': {
+        'row_parsers': {
+            "Weight Tracker": weight_tracker_parser
+        },
+        'completer': weight_tracker_complete_row
+    }
 }
 
-def find_row_parser_for(filename):
-    for pattern, parser in row_parsers.iteritems():
+def find_row_parser_for(file_type, filename):
+    for pattern, parser in file_type_handlers[file_type]['row_parsers'].iteritems():
         if re.search(pattern, filename):
             return parser
     return None
@@ -27,9 +52,15 @@ def find_row_parser_for(filename):
 def iso8601_date(timestamp):
     return timestamp.replace('/', '-')[0:10]
 
+def deduce_file_type_from_headers(headers):
+    if 'Kg' in headers:
+        return 'weight'
+    return 'unknown'
+
 def main():
     by_date = {}
     parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--type")
     parser.add_argument("-o", "--output")
     parser.add_argument("mainfile")
     parser.add_argument('incoming', nargs='+')
@@ -46,6 +77,10 @@ def main():
         for row in probereader:
             fieldnames = row
             break               # read only the first row
+    if args.type is None:
+        file_type = deduce_file_type_from_headers(fieldnames)
+    else:
+        file_type = args.type
     with open(args.mainfile) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
@@ -53,7 +88,7 @@ def main():
             by_date[row_date] = row
         csvfile.close()
     for incoming_file in args.incoming:
-        row_parser = find_row_parser_for(incoming_file)
+        row_parser = find_row_parser_for(file_type, incoming_file)
         if row_parser is None:
             print "Skipping", incoming_file, "as I don't have a parser for it"
         else:
@@ -69,10 +104,14 @@ def main():
                                 existing_row[key] = value
                         else:
                             by_date[new_row_date] = new_row
+    completer = file_type_handlers[file_type]['completer']
+    sorted_dates = sorted(by_date.keys())
+    for date in sorted_dates:
+        completer(by_date[date])
     with open(output, 'w') as outstream:
         writer = csv.DictWriter(outstream, fieldnames)
         writer.writeheader()
-        for date in sorted(by_date.keys()):
+        for date in sorted_dates:
             writer.writerow(by_date[date])
 
 if __name__ == "__main__":
