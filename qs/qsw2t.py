194a6c2990f91baae94eaea748a25ed36bc3c6ad
John Sturdy <jcg.sturdy@gmail.com>
2017-09-24 22:12:49 +0100

    Fill in actual program
    It seems to work in this form, although it doesn't explictly output
    the empty comment string at the end of each row.

--- qs/qsw2t.py
+++ qs/qsw2t.py
@@ -15,9 +15,29 @@ def main():
     parser.add_argument("-e", "--existing",
                         help="""The name of a file containing the existing app entries as exported by the app.""")
     parser.add_argument("-o", "--output",
+                        required=True,
                         help="""The name of the file to put the output in.""")
     parser.add_argument("data",
                         help="""The main data file.""")
+    args = parser.parse_args()
+    exclude = {}
+    output = []
+    if args.existing is not None:
+        with open(args.existing) as existing:
+            for row in csv.reader(existing):
+                if len(row) >= 2:
+                    exclude[row[1][:10]] = row[0]
+    with open(args.data) as main_file:
+        with open(args.output, 'w') as outfile:
+            app_writer = csv.writer(outfile)
+            for row in csv.DictReader(main_file):
+                date = row['Date']
+                if date in exclude:
+                    continue
+                weight = row['Kg']
+                if weight == "":
+                    continue
+                app_writer.writerow([weight, date + " 08:00:00", ""])
 
 if __name__ == "__main__":
     main()
