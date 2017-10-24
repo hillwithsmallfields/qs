72bdeffc38f5bbb39c6e1eebf277e8171c716f2b
John Sturdy <jcg.sturdy@gmail.com>
2017-10-24 21:31:39 +0100

    First steps towards updating existing files
    Also a tiny bit towards multiple accounts in one file.

--- qs/finconv.py
+++ qs/finconv.py
@@ -21,6 +21,8 @@ import yaml
 #       payee: Details
 #       credits: Money in
 #       debits: Money out
+#     currency: "GBP"
+#     name: "Handelsbanken current account"
 #     conversions:
 #       "MYEMPLOYER LTD":
 #         'category': "Salary"
@@ -36,16 +38,28 @@ import yaml
 #       date: date
 #       payee: payee
 
+def deduce_format(first_row, formats):
+    condensed_row = [cell for cell in first_row if cell != ""]
+    for format_name, format_def in formats.iteritems():
+        sequence = [col for col in format_def['column-sequence'] if col]
+        if sequence == condensed_row:
+            return format_name
+        return None
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config",
                         default="~/.qs-conf.yaml")
     parser.add_argument("-f", "--format",
                         default='handelsbanken')
-    parser.add_argument("-o", "--output")
     parser.add_argument("-O", "--output-format",
                         default='financisto')
-    parser.add_argument("input_file")
+
+    outfile_handling = parser.add_mutually_exclusive_group(required=True)
+    outfile_handling.add_argument("-o", "--output")
+    output.add_argument("-u", "--update")
+
+    parser.add_argument("input_files", nargs='*')
     args = parser.parse_args()
 
     with open(os.path.expanduser(os.path.expandvars(args.config))) as config_file:
@@ -53,24 +67,45 @@ def main():
 
     print "config is", config
 
-    input_format = config['formats'][args.format]
-    in_date = input_format['columns']['date']
-    in_payee = input_format['columns']['payee']
-    in_credits = input_format['columns']['credits']
-    in_debits = input_format['columns']['debits']
-    conversions = input_format['conversions']
+    if args.update:
+        infile_names = [args.update] + args.input_files
+        outfile = args.update
+    else:
+        infile_names = args.input_files
+        outfile = args.output
+        output_format_name = args.output_format
 
-    output_format = config['formats'][args.output_format]
-    out_date = output_format['columns']['date']
-    out_payee = output_format['columns']['payee']
+    print "input files are", infile_names
+    print "outfile is", outfile
 
-    print "input format is", input_format
-    print "output format is", output_format
+    output_rows = {}
+    first_file = True
 
-    with open(os.path.expanduser(os.path.expandvars(args.output)), 'w') as outfile:
-        writer = csv.DictWriter(outfile, output_format['column-sequence'])
-        writer.writeheader()
-        with open(os.path.expanduser(os.path.expandvars(args.input_file))) as infile:
+    for input_file_name in infile_names:
+        with open(os.path.expanduser(os.path.expandvars(input_file_name))) as infile:
+            sample_row = csv.Reader(infile)
+            input_format = deduce_format(sample_row, config['formats']) or config['formats'][args.format]
+            print "input format for", input_file_name, "is", input_format
+            if first_file:
+                first_file = False
+                if args.update:
+                    output_format_name = input_format
+                output_format = config['formats'][output_format_name]
+                out_date = output_format['columns']['date']
+                out_payee = output_format['columns']['payee']
+                out_name = output_format['name']
+                out_currency = output_format['currency']
+                print "output format is", output_format
+        in_date = input_format['columns']['date']
+        in_payee = input_format['columns']['payee']
+        in_credits = input_format['columns']['credits']
+        if not isinstance(in_credits, basestring):
+            pass                # todo: further lookup for multi-account files
+        in_debits = input_format['columns']['debits']
+        if not isinstance(in_debits, basestring):
+            pass                # todo: further lookup for multi-account files
+        conversions = input_format['conversions']
+        with open(os.path.expanduser(os.path.expandvars(input_file_name))) as infile:
             for row in csv.DictReader(infile):
                 conversion = conversions.get(row[in_payee], None)
                 if conversion:
@@ -78,19 +113,27 @@ def main():
                     money_in = 0 if money_in == '' else float(money_in)
                     money_out = row[in_debits]
                     money_out = 0 if money_out == '' else float(money_out)
-                    writer.writerow({out_date: row[in_date],
-                                     'time': "08:00:00",
-                                     'account': "Handelsbanken current account",
-                                     'amount': money_in - money_out,
-                                     'currency': "GBP",
-                                     'original amount': "",
-                                     'original currency': "",
-                                     'category': conversion['category'],
-                                     'parent': conversion['parent'],
-                                     out_payee: conversion['payee'],
-                                     'location': "",
-                                     'project': "",
-                                     'note': "Imported from bank statement"})
+                    row_date = row[in_date]
+                    row_time = "08:00:00"
+                    output_rows[row_date+"T"+row_time] = {out_date: row_date,
+                                                          'time': row_time,
+                                                          'account': out_name, # was "Handelsbanken current account"
+                                                          'amount': money_in - money_out,
+                                                          'currency': out_currency, # was "GBP"
+                                                          'original amount': "",
+                                                          'original currency': "",
+                                                          'category': conversion['category'],
+                                                          'parent': conversion['parent'],
+                                                          out_payee: conversion['payee'],
+                                                          'location': "",
+                                                          'project': "",
+                                                          'note': "Imported from bank statement"}
+
+    with open(os.path.expanduser(os.path.expandvars(outfile)), 'w') as outfile:
+        writer = csv.DictWriter(outfile, output_format['column-sequence'])
+        writer.writeheader()
+        for timestamp in sorted(output_rows.keys()):
+            writer.writerow(output_rows[timestamp])
 
 if __name__ == "__main__":
     main()
