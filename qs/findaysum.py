d8fca263f6c542141ee1c60802af014733809098
John Sturdy <jcg.sturdy@gmail.com>
2017-10-27 17:47:40 +0100

    Initial attempt at the summing logic

--- qs/findaysum.py
+++ qs/findaysum.py
@@ -24,5 +24,34 @@ def main():
 
     print "config is", config
 
+    days = {}
+    
+    with open(os.path.expanduser(os.path.expandvars(args.input_file))) as infile:
+        input_format = deduce_format(sample_row, config['formats']) or config['formats'][args.format]
+        print "input format for", input_file_name, "is", input_format
+        in_columns = input_format['columns']
+        in_date = in_columns['date']
+        in_payee = in_columns['payee']
+        account_columns = input_format['accounts']
+        for row in csv.DictReader(infile):
+            date = row[in_date]
+            day_data = days[date] = days.get[date, {in_date: date})
+            payee_name = row.get(in_payee, "unknown")
+            payee_accounts = day_data[payee_name] = day_data.get(payee_name, {})
+            for account_name in account_columns:
+                payee_accounts[account_name] = (payee_accounts.get(account_name, 0)
+                                                + float(row.get(account_name, "0")))
+                        
+    with open(os.path.expanduser(os.path.expandvars(outfile)), 'w') as outfile:
+        writer = csv.DictWriter(outfile, output_formato['column-sequence'])
+        writer.writeheader()
+        for date in sorted(days.keys()):
+            day_data = days[date]
+            for payee_name in day_data.keys().sorted():
+                out_row = {in_date: date, in_payee: payee_name}
+                for account_name, account_change in day_data[payee_name].iteritems():
+                    out_row[account_name] = account_change
+                writer.writerow(out_row)
+                
 if __name__ == "__main__":
     main()
