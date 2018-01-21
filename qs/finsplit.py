d21d99858a52960b12f352d0eb5d000131e7659f
John Sturdy <jcg.sturdy@gmail.com>
2018-01-21 21:59:20 +0000

    Write the splitter functions

--- qs/finsplit.py
+++ qs/finsplit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Program to split accounts into a separate column per category
+# Program to split accounts into a separate column per category or payee
 
 import argparse
 import csv
@@ -10,6 +10,28 @@ import qsutils
 # See notes in finconv.py for config file format
 
 DEFAULT_CONF = "/usr/local/share/qs-accounts.yaml"
+
+def finsplit_setup(args, config, input_format):
+    in_columns = input_format['columns']
+    print "in_columns are", in_columns
+    return ['date'], {'split_by': input_format['columns']['payee'
+                                                          if args.payee
+                                                          else 'category'],
+                      'verticals': set(()),
+                      'date_column_name': in_columns['date'],
+                      'amount_column': in_columns['amount'][args.account]}
+
+def finsplit_row(timestamp, row, output_rows, scratch):
+    decider = row[scratch['split_by']]
+    scratch['verticals'].add(decider)
+    row_date = row[scratch['date_column_name']]
+    amount = row[scratch['amount_column']]
+    output_row = {'date': row_date,
+                  decider: amount}
+    output_rows[row_date] = output_row
+
+def finsplit_tidyup(columns, rows, scratch):
+    return columns + sorted(scratch['verticals']), rows
 
 def main():
     parser = argparse.ArgumentParser()
@@ -23,9 +45,16 @@ def main():
     parser.add_argument("-o", "--output")
     parser.add_argument("-f", "--format",
                         default=None)
+    parser.add_argument("-a", "--account",
+                        help="The account column to split.")
+    split_by = parser.add_mutually_exclusive_group(required=True)
+    split_by.add_argument("-C", "--category", action='store_true')
+    split_by.add_argument("-P", "--payee", action='store_true')
     parser.add_argument("input_file")
 
     args = parser.parse_args()
+
+    print "args are", args
 
     config_files = ([DEFAULT_CONF]
                     if os.path.exists(DEFAULT_CONF) and not args.no_default_config
@@ -36,6 +65,7 @@ def main():
 
     config = qsutils.load_config(args.verbose, *config_files)
 
-    # todo: deduce format of input file; should normally be financisto, or have similar data
+    qsutils.process_fin_csv_rows(args, config, finsplit_setup, finsplit_row, finsplit_tidyup)
 
-    # todo: scan input file making a new set of columns, one for each category, and using the category column to distribute the data among them
+if __name__ == "__main__":
+    main()
