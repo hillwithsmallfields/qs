eeab714854762aad421ac6a59e25790aee6c3410
John Sturdy <jcg.sturdy@gmail.com>
2019-02-27 07:49:51 +0000

    improve sibling finding
    Also more analysis, and add a couple of columns

--- contacts/link-contacts.py
+++ contacts/link-contacts.py
@@ -4,9 +4,11 @@ import argparse
 from backports import csv
 import io
 import random
+import operator
 
 fieldnames = ['Given name', 'Middle names', 'Surname', 'Title', 'Old name', 'AKA',
-              'Birthday',
+              'Birthday', 'Died',
+              'First contact', 'Last contact',
               'Gender',
               'ID', 'Parents', 'Offspring', 'Siblings', 'Partners', 'Ex-partners', 'Nationality',
               'Notes',
@@ -42,8 +44,26 @@ def partners(person):
 def siblings(person):
     return person['Siblings']
 
+def find_siblings(person, by_id):
+    sibs = siblings(person)
+    for sib in siblings(person):
+        print "Adding sibs of", sib
+        sibsibs = siblings(by_id[sib])
+        print "Who are", sibsibs
+        for sibsib in sibsibs:
+            if sibsib not in sibs:
+                sibs.append(sibsib)
+        print "sibs is now", sibs
+    yourself = person['ID']
+    if yourself in sibs:
+        sibs.remove(yourself)
+    return sibs
+
 def name(person):
     return person['_name_']
+
+def group_titles(title_map, titles):
+    return reduce(operator.add, map(len, map(lambda(title): title_map[title], titles)))
 
 def main():
     parser = argparse.ArgumentParser()
@@ -93,10 +113,15 @@ def main():
         by_title[title].append(id)
 
     if args.analyze:
-        print len(by_id), "people"
+        n_people = len(by_id)
+        print n_people, "people"
         print len(by_nationality), "nationalities:", ", ".join([k + "(" + str(len(by_nationality[k])) + ")" for k in sorted(by_nationality.keys())])
         print len(by_gender), "genders:", ", ".join([k + "(" + str(len(by_gender[k])) + ")" for k in sorted(by_gender.keys())])
         print len(by_title), "titles:", ", ".join([k + "(" + str(len(by_title[k])) + ")" for k in sorted(by_title.keys())])
+        ordained = group_titles(by_title, ["Revd", "Revd Dr", "Revd Prof", "Rt Revd"])
+        doctored = group_titles(by_title, ["Dr", "Revd Dr", "Prof", "Revd Prof"])
+        print "%d ordained (%d%% of the people you know)" % (ordained, ordained*100 / n_people)
+        print "%d with doctorates (%d%% of the people you know)" % (doctored, doctored * 100 / n_people)
 
     for nm in sorted(by_name.keys()):
         person = by_name[nm]
@@ -109,7 +134,7 @@ def main():
             child = by_id[offspring_id]
             if person_id not in parents(child):
                 parents(child).append(person_id)
-        for sibling_id in siblings(person):
+        for sibling_id in find_siblings(person, by_id):
             sibling = by_id[sibling_id]
             if person_id not in siblings(sibling):
                 siblings(sibling).append(person_id)
