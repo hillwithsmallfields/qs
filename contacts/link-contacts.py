f945518b00b4bf8069c4f04b5418f3315410ebae
John Sturdy <jcg.sturdy@gmail.com>
2019-03-05 12:25:58 +0000

    Transform names in cross-linking columns into IDs
    Also some tidying, including removing tracing

--- contacts/link-contacts.py
+++ contacts/link-contacts.py
@@ -4,6 +4,7 @@ import argparse
 from backports import csv
 import io
 import random
+import re
 import operator
 
 fieldnames = ['Given name', 'Middle names', 'Surname', 'Title', 'Old name', 'AKA',
@@ -45,16 +46,21 @@ def partners(person):
 def siblings(person):
     return person['Siblings']
 
+by_name = {}
+
+def normalize_to_IDs(people):
+    return [ (person
+              if re.match("[G-Z][0-9][A-Z][0-9]", person)
+              else by_name[person.replace('_', ' ')]['ID'])
+             for person in people ]
+
 def find_siblings(person, by_id):
     sibs = siblings(person)
     for sib in siblings(person):
-        print "Adding sibs of", sib
         sibsibs = siblings(by_id[sib])
-        print "Who are", sibsibs
         for sibsib in sibsibs:
             if sibsib not in sibs:
                 sibs.append(sibsib)
-        print "sibs is now", sibs
     yourself = person['ID']
     if yourself in sibs:
         sibs.remove(yourself)
@@ -72,9 +78,9 @@ def main():
     parser.add_argument("input")
     parser.add_argument("output")
     args = parser.parse_args()
+    global by_name
     by_id = {}
     without_id = []
-    by_name = {}
     by_nationality = {}
     by_gender = {}
     by_title = {}
@@ -99,34 +105,13 @@ def main():
         person['ID'] = id
         by_id[id] = person
 
-    for id, person in by_id.iteritems():
-        nationality = person['Nationality']
-        if nationality not in by_nationality:
-            by_nationality[nationality] = []
-        by_nationality[nationality].append(id)
-        gender = person['Gender']
-        if gender not in by_gender:
-            by_gender[gender] = []
-        by_gender[gender].append(id)
-        title = person['Title']
-        if title not in by_title:
-            by_title[title] = []
-        by_title[title].append(id)
+    for person in by_id.values():
+        person['Parents'] = normalize_to_IDs(parents(person))
+        person['Offspring'] = normalize_to_IDs(offspring(person))
+        person['Siblings'] = normalize_to_IDs(siblings(person))
+        person['Partners'] = normalize_to_IDs(partners(person))
 
-    if args.analyze:
-        n_people = len(by_id)
-        print n_people, "people"
-        print len(by_nationality), "nationalities:", ", ".join([k + "(" + str(len(by_nationality[k])) + ")" for k in sorted(by_nationality.keys())])
-        print len(by_gender), "genders:", ", ".join([k + "(" + str(len(by_gender[k])) + ")" for k in sorted(by_gender.keys())])
-        print len(by_title), "titles:", ", ".join([k + "(" + str(len(by_title[k])) + ")" for k in sorted(by_title.keys())])
-        ordained = group_titles(by_title, ["Revd", "Revd Dr", "Revd Prof", "Rt Revd"])
-        doctored = group_titles(by_title, ["Dr", "Revd Dr", "Prof", "Revd Prof"])
-        print "%d ordained (%d%% of the people you know)" % (ordained, ordained*100 / n_people)
-        print "%d with doctorates (%d%% of the people you know)" % (doctored, doctored * 100 / n_people)
-
-    for nm in sorted(by_name.keys()):
-        person = by_name[nm]
-        person_id = person['ID']
+    for person_id, person in by_id.iteritems():
         for parent_id in parents(person):
             parent = by_id[parent_id]
             if person_id not in offspring(parent):
@@ -139,6 +124,30 @@ def main():
             sibling = by_id[sibling_id]
             if person_id not in siblings(sibling):
                 siblings(sibling).append(person_id)
+
+    if args.analyze:
+        for id, person in by_id.iteritems():
+            nationality = person['Nationality']
+            if nationality not in by_nationality:
+                by_nationality[nationality] = []
+            by_nationality[nationality].append(id)
+            gender = person['Gender']
+            if gender not in by_gender:
+                by_gender[gender] = []
+            by_gender[gender].append(id)
+            title = person['Title']
+            if title not in by_title:
+                by_title[title] = []
+            by_title[title].append(id)
+        n_people = len(by_id)
+        print n_people, "people"
+        print len(by_nationality), "nationalities:", ", ".join([k + "(" + str(len(by_nationality[k])) + ")" for k in sorted(by_nationality.keys())])
+        print len(by_gender), "genders:", ", ".join([k + "(" + str(len(by_gender[k])) + ")" for k in sorted(by_gender.keys())])
+        print len(by_title), "titles:", ", ".join([k + "(" + str(len(by_title[k])) + ")" for k in sorted(by_title.keys())])
+        ordained = group_titles(by_title, ["Revd", "Revd Dr", "Revd Prof", "Rt Revd"])
+        doctored = group_titles(by_title, ["Dr", "Revd Dr", "Prof", "Revd Prof"])
+        print "%d ordained (%d%% of the people you know)" % (ordained, ordained*100 / n_people)
+        print "%d with doctorates (%d%% of the people you know)" % (doctored, doctored * 100 / n_people)
 
     with io.open(args.output, 'w', encoding='utf-8') as output:
         contacts_writer = csv.DictWriter(output, fieldnames)
