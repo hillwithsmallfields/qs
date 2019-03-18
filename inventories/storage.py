3e8a5d711aba49f7f9ea4c61f8d52d2c837daf80
John Sturdy <jcg.sturdy@gmail.com>
2019-03-18 20:50:05 +0000

    Change how commands work
    It now has a simple command interpreter.
    
    Also, start work on the command for listing what is in a given location.

--- inventories/storage.py
+++ inventories/storage.py
@@ -5,6 +5,7 @@ import io
 import operator
 import os
 import re
+import sys
 
 def read_books(books_file):
     books = {}
@@ -29,9 +30,10 @@ def books_matching(book_index, pattern):
              for book in book_index.values()
              if book_matches(book, pattern) ]
 
-def list_books(locations, books):
+def list_books(outstream, args, locations, items, books):
+    """Show a table of where all the books are."""
     # todo: print table of where all books are
-    pass
+    return True
 
 def read_inventory(inventory_file):
     inventory = {}
@@ -58,9 +60,10 @@ def items_matching(inventory_index, pattern):
              for item in inventory_index.values()
              if item_matches(item, pattern) ]
 
-def list_items(locations, items):
+def list_items(outstream, args, locations, items, books):
+    """Show a table of where all the items are."""
     # todo: option to print table of where all inventory items are
-    pass
+    return True
 
 def read_locations(locations_file):
     locations = {}
@@ -73,6 +76,7 @@ def read_locations(locations_file):
                                       else None)
             number = row['Number']
             if number != "":
+                row['Number'] = number
                 locations[int(number)] = row
     return locations
 
@@ -109,7 +113,10 @@ def describe_location(locations, location):
             if location != ""
             else "unknown")
 
-def analyze_locations(locations):
+def capacities(outstream, args, locations, items, books):
+    """Analyze the storage capacities.
+Shows how much of each type of storage there is, and also a summary
+combining the types."""
     capacities = {}
     for location in locations.values():
         loctype = location['Type'].lower()
@@ -117,7 +124,8 @@ def analyze_locations(locations):
         if loctype != "" and locsize != "":
             capacities[loctype] = float(capacities.get(loctype, 0)) + float(locsize)
     for loctype in sorted(capacities.keys()):
-        print loctype.rjust(max(*map(len, capacities.keys()))), capacities[loctype]
+        label_width = max(*map(len, capacities.keys()))
+        outstream.write(loctype.rjust(label_width) + " " + str(capacities[loctype]) + "\n")
     volume = reduce(operator.add,
                     map(lambda loctype: capacities.get(loctype, 0),
                         ('box', 'crate', 'drawer', 'cupboard')))
@@ -127,13 +135,142 @@ def analyze_locations(locations):
     area = reduce(operator.add,
                   map(lambda loctype: capacities.get(loctype, 0),
                         ('louvre panel', 'pegboard')))
-    print "Total container volume:", volume, "litres"
-    print "Total shelving length:", length, "metres"
-    print "Total panel area:", area, "square metres"
+    outstream.write("Total container volume: " + str(volume) + " litres\n")
+    outstream.write("Total shelving length: " + str(length) + " metres\n")
+    outstream.write("Total panel area: " + str(area) + " square metres\n")
+    return True
 
-def list_locations(locations):
-    # todo: option to print table of all storage locations, with everything that is in them
-    pass
+def construct_surrounders(locations):
+    s = {}
+    for loc_num, loc_descr in locations.iteritems():
+        around = loc_descr['ContainedWithin']
+        if around:
+            if around in s:
+                s[around].append(loc_num)
+            else:
+                s[around] = [loc_num]
+    return s
+
+def add_contained_storage(matching_locations, locations, surrounders, loc_number):
+    if loc_number in surrounders:
+        for inner_container in surrounders[loc_number]:
+            matching_locations[inner_container] = locations[inner_container]
+            add_contained_storage(matching_locations, locations, surrounders, inner_container)
+
+surrounders = None
+
+def list_locations(outstream, things, locations, items, books):
+    """List everything that is in the matching locations.
+When complete, this should list them hierarchically."""
+    global surrounders
+    # todo: make this list things hierarchically (as it goes, in one loop, instead of saving all up for the end)
+    if surrounders is None:
+        surrounders = construct_surrounders(locations)
+    matching_locations = {}
+    for loc_name in things:
+        loc_match = re.compile(loc_name, re.IGNORECASE)
+        for loc in locations.values():
+            if loc_match.search(loc['Description']):
+                loc_number = int(loc['Number'])
+                matching_locations[loc_number] = loc
+                add_contained_storage(matching_locations, locations, surrounders, loc_number)
+    for loc_number in sorted(matching_locations.keys()):
+        things_here = []
+        for thing in items.values():
+            if thing['Normal_location'] == loc_number:
+                things_here.append(thing)
+        # todo: add books
+        location = matching_locations[loc_number]
+        description = location['Description']
+        level = location['Level']
+        if level != "":
+            description += " level " + str(level)
+        if len(things_here) == 0:
+            outstream.write(description + " has no known direct contents\n")
+        else:
+            outstream.write(description + " contains\n")
+            for what in things_here:
+                outstream.write("    " + what['Item'] + "\n")
+    return True
+
+def find_things(outstream, args, locations, items, books):
+    """Show the locations of things.
+This finds books, other items, and locations."""
+    findings = {}
+    for thing in args:
+        if re.match("[0-9]+", thing):
+            as_location = describe_location(locations, thing)
+            if as_location != []:
+                findings[thing] = as_location
+        for book in books_matching(books, thing):
+            findings[book['Title']] = describe_location(locations, book['Location'])
+        for item in items_matching(items, thing):
+            findings[item['Item']] = describe_location(locations, item['Normal_location'])
+    for finding in sorted(findings.keys()):
+        outstream.write(finding + " is " + findings[finding] + "\n")
+    return True
+
+def cmd_help(outstream, args, locations, items, books):
+    """Display some help."""
+    if len(args) == 0:
+        outstream.write("Commands are:\n")
+        max_command_length = max(*map(len, commands.keys()))
+        for command in sorted(commands.keys()):
+            outstream.write("  " + command.rjust(max_command_length)
+                            + ": " + commands[command].__doc__.split('\n')[0] + "\n")
+    else:
+        for topic in args:
+            if topic in commands:
+                outstream.write(topic + ":\n" + commands[topic].__doc__ + "\n")
+            else:
+                outstream.write(topic + ":\n" + topic
+                                + """ is not a command.  Type "help" for a list of commands.\n""")
+    return True
+
+def cmd_quit(outstream, args, locations, items, books):
+    """Stop the CLI."""
+    return False
+
+def cmd_bad(outstream, args, locations, items, books):
+    """Report an invalid command."""
+    outstream.write("""Bad command; enter "help" to get a list of commands\n""")
+    return True
+
+commands = {
+    'where': find_things,
+    'items': list_items,
+    'books': list_books,
+    'what': list_locations,
+    'capacities': capacities,
+    'help': cmd_help,
+    'quit': cmd_quit
+}
+
+def run_command(outstream,
+                command,
+                things,
+                locations,
+                items, books):
+    return commands.get(command, cmd_bad)(outstream,
+                                          things,
+                                          locations,
+                                          items,
+                                          books)
+
+def cli(instream, outstream, prompt, locations, items, books):
+    """Run a command loop using stdin and stdout."""
+    while True:
+        if prompt:
+            outstream.write(prompt)
+        line_parts = instream.readline().strip().split()
+        if len(line_parts) == 0:
+            continue
+        if not run_command(outstream,
+                           line_parts[0],
+                           line_parts[1:],
+                           locations,
+                           items, books):
+            break
 
 def main():
     parser = argparse.ArgumentParser()
@@ -147,46 +284,29 @@ def main():
     parser.add_argument("--inventory", "-i",
                         default=os.path.join(org_files, "inventory.csv"),
                         help="""The CSV file containing the inventory.""")
-    parser.add_argument("--list-books", action='store_true',
-                        help="""List all the books.""")
-    parser.add_argument("--list-items", action='store_true',
-                        help="""List all the inventory items.""")
-    parser.add_argument("--list-locations", action='store_true',
-                        help="""List all the storage locations.""")
-    parser.add_argument("--analyze", action='store_true',
-                        help="""Analyze the storage.""")
+    actions = parser.add_mutually_exclusive_group()
+    actions.add_argument("--server", action='store_true',
+                        help="""Run a little CLI on a network socket.""")
+    actions.add_argument("--cli", action='store_true',
+                         help="""Run a little CLI on stdin and stdout.""")
     parser.add_argument("things",
                         nargs='*',
                         help="""The things to look for.""")
     args = parser.parse_args()
     locations = read_locations(args.locations)
-    inventory = read_inventory(args.inventory)
+    items = read_inventory(args.inventory)
     books = read_books(args.books)
-    if args.analyze:
-        analyze_locations(locations)
-    if args.list_books:
-        list_books(locations, books)
-    elif args.list_items:
-        list_items(locations, items)
-    elif args.list_locations:
-        list_locations(locations)
+    if args.cli:
+        cli(sys.stdin, sys.stdout, "storage> ", locations, items, books)
+    elif args.server:
+        pass # cli(inputsocket, outputsocket, None, locations, items, books)
     else:
-        # todo: collect up all the books/items matching all the "thing"s, eliminating duplicates, then output them, as currently if something matches more than one "thing", it appears more than once in the output
-        for thing in args.things:
-            if re.match("[0-9]+", thing):
-                as_location = describe_location(locations, thing)
-                if as_location != []:
-                    print "as location:", as_location
-                # todo: also look up books and items by number
-            for book in books_matching(books, thing):
-                shelf = book['Location']
-                where = describe_location(locations, shelf)
-                print "book:", book['Title'], "is on", where
-            for item in items_matching(inventory, thing):
-                shelf = item['Normal_location']
-                where = describe_location(locations, shelf)
-                print "item:", item['Item'], "is", where
-            # todo: try each name as a storage location name, and if found, list everything in that location
+        if args.things[0] in commands:
+            run_command(sys.stdout, args.things[0], args.things[1:],
+                        locations, items, books)
+        else:
+            run_command(sys.stdout, "where", args.things,
+                        locations, items, books)
 
 if __name__ == "__main__":
     main()
