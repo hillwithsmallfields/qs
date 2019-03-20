f9e6c28d7d277bc5392b0376816f75359b5919f5
John Sturdy <jcg.sturdy@gmail.com>
2019-03-20 08:11:22 +0000

    Add some files I had forgotten to add earlier

---
+++ qs/finperiodic.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+# Program to detect periodic payments and spot gaps in them
+
+import argparse
+import csv
+import os
+import qsutils
+
+# See notes in finconv.py for config file format
+
+DEFAULT_CONF = "/usr/local/share/qs-accounts.yaml"
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config",
+                        action='append')
+    parser.add_argument("-n", "--no-default-config",
+                        action='store_true',
+                        help="""Do not load the default config file.""")
+    parser.add_argument("-v", "--verbose",
+                        action='store_true')
+    parser.add_argument("-o", "--output")
+    parser.add_argument("-f", "--format",
+                        default=None)
+    parser.add_argument("input_file")
+
+    args = parser.parse_args()
+
+    config_files = ([DEFAULT_CONF]
+                    if os.path.exists(DEFAULT_CONF) and not args.no_default_config
+                    else [])
+
+    if args.config:
+        config_files += args.config
+
+    config = qsutils.load_config(args.verbose, *config_files)
+
+    # todo: deduce format of input file; should normally be financisto, or have similar data
+
+
+if __name__ == "__main__":
+    main()
