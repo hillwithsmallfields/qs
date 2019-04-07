89c080dfe2b02cbe6e268d53c3f1bc085b4a915d
John Sturdy <jcg.sturdy@gmail.com>
2019-04-07 12:04:42 +0200

    Tidy up a bit, and split.
    Split the reading and writing code out into a separate file, ready for
    using it from a separate program for producing address lists.

--- contacts/link-contacts.py
+++ contacts/link-contacts.py
@@ -1,41 +1,8 @@
 #!/usr/bin/env python
 
 import argparse
-from backports import csv
-import io
-import random
 import re
-import operator
-
-fieldnames = ['Given name', 'Middle names', 'Surname', 'Title', 'Old name', 'AKA',
-              'Birthday', 'Died',
-              'First contact', 'Last contact',
-              'Gender',
-              'ID', 'Parents', 'Offspring', 'Siblings', 'Partners', 'Ex-partners', 'Knows', 'Nationality',
-              'Notes',
-              'Group Membership', 'Flags', 'Other groups', 'Organizations', 'Place met',
-              'Subjects', 'Jobs',
-              'Primary email', 'Other emails',
-              'Primary phone Type', 'Primary phone Value',
-              'Secondary phone Type', 'Secondary phone Value',
-              'Street', 'City', 'Region', 'Postal Code', 'Country', 'Extended Address']
-
-# Fields to split into lists
-multi_fields = ['Parents', 'Offspring', 'Siblings',
-                'Partners', 'Ex-partners',
-                'Knows',
-                'Organizations']
-
-def make_name(person):
-    return ' '.join([person.get('Given name', "")]
-                    + person.get('Middle names', "").split()
-                    + [person.get('Surname', "")])
-
-def make_ID():
-    return (str(unichr(random.randint(0, 19) + ord('G')))
-            + str(unichr(random.randint(0, 9) + ord('0')))
-            + str(unichr(random.randint(0, 25) + ord('A')))
-            + str(unichr(random.randint(0, 9) + ord('0'))))
+import contacts_data
 
 def offspring(person):
     return person['Offspring']
@@ -49,9 +16,7 @@ def partners(person):
 def siblings(person):
     return person['Siblings']
 
-by_name = {}
-
-def normalize_to_IDs(people):
+def normalize_to_IDs(people, by_name):
     return [ (person
               if re.match("[G-Z][0-9][A-Z][0-9]", person)
               else by_name[person.replace('_', ' ')]['ID'])
@@ -72,9 +37,6 @@ def find_siblings(person, by_id):
 def name(person):
     return person['_name_']
 
-def group_titles(title_map, titles):
-    return reduce(operator.add, map(len, map(lambda(title): title_map[title], titles)))
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--analyze", action='store_true')
@@ -82,39 +44,18 @@ def main():
     parser.add_argument("input")
     parser.add_argument("output")
     args = parser.parse_args()
-    global by_name
-    by_id = {}
-    without_id = []
     by_nationality = {}
     by_gender = {}
     by_title = {}
-    with io.open(args.input, 'r', encoding='utf-8') as input:
-        contacts_reader = csv.DictReader(input)
-        for row in contacts_reader:
-            for multi in multi_fields:
-                row[multi] = (row.get(multi, "") or "").split()
-            n = make_name(row)
-            row['_name_'] = n
-            by_name[n] = row
-            id = row.get('ID', "")
-            if id is not None and id != "":
-                by_id[id] = row
-            else:
-                without_id.append(row)
 
-    for person in without_id:
-        id = make_ID()
-        while id in by_id:
-            id = make_ID()
-        person['ID'] = id
-        by_id[id] = person
+    by_id, by_name = contacts_data.read_contacts(args.input)
 
     for person in by_id.values():
-        person['Parents'] = normalize_to_IDs(person['Parents'])
-        person['Offspring'] = normalize_to_IDs(person['Offspring'])
-        person['Siblings'] = normalize_to_IDs(person['Siblings'])
-        person['Partners'] = normalize_to_IDs(person['Partners'])
-        person['Knows'] = normalize_to_IDs(person['Knows'])
+        person['Parents'] = normalize_to_IDs(person['Parents'], by_name)
+        person['Offspring'] = normalize_to_IDs(person['Offspring'], by_name)
+        person['Siblings'] = normalize_to_IDs(person['Siblings'], by_name)
+        person['Partners'] = normalize_to_IDs(person['Partners'], by_name)
+        person['Knows'] = normalize_to_IDs(person['Knows'], by_name)
 
     for person_id, person in by_id.iteritems():
         partner_ids = person['Partners']
@@ -156,8 +97,8 @@ def main():
         print len(by_nationality), "nationalities:", ", ".join([k + "(" + str(len(by_nationality[k])) + ")" for k in sorted(by_nationality.keys())])
         print len(by_gender), "genders:", ", ".join([k + "(" + str(len(by_gender[k])) + ")" for k in sorted(by_gender.keys())])
         print len(by_title), "titles:", ", ".join([k + "(" + str(len(by_title[k])) + ")" for k in sorted(by_title.keys())])
-        ordained = group_titles(by_title, ["Revd", "Revd Dr", "Revd Prof", "RtRevd"])
-        doctored = group_titles(by_title, ["Dr", "Revd Dr", "Prof", "Revd Prof"])
+        ordained = contacts_data.count_grouped_titles(by_title, ["Revd", "Revd Dr", "Revd Prof", "RtRevd"])
+        doctored = contacts_data.count_grouped_titles(by_title, ["Dr", "Revd Dr", "Prof", "Revd Prof"])
         print "%d ordained (%d%% of the people you know)" % (ordained, ordained*100 / n_people)
         print "%d with doctorates (%d%% of the people you know)" % (doctored, doctored * 100 / n_people)
 
@@ -179,16 +120,7 @@ def main():
                 print "    ", id, "->", "{", ",".join(their_parents), "} [style=dashed]"
         print "}"
 
-    with io.open(args.output, 'w', encoding='utf-8') as output:
-        contacts_writer = csv.DictWriter(output, fieldnames)
-        contacts_writer.writeheader()
-        for nm in sorted(by_name.keys()):
-            row = by_name[nm]
-            # print row
-            for multi in multi_fields:
-                row[multi] = ' '.join(row[multi])
-            del row['_name_']
-            contacts_writer.writerow(row)
+    contacts_data.write_contacts(args.output, by_name)
 
 if __name__ == "__main__":
     main()
