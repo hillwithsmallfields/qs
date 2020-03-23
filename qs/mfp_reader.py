a7e8a26d7a6d627d8cf5084723b7ab28d64c1456
John Sturdy <jcg.sturdy@gmail.com>
2020-03-23 23:35:01 +0000

    Add the main part of my myfitnesspal reader

---
+++ qs/mfp_reader.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# program to scrape myfitnesspal, using https://github.com/coddingtonbear/python-myfitnesspal
+
+from decouple import config
+import datetime
+import json
+import myfitnesspal
+import random
+import time
+
+client = myfitnesspal.Client(config('MFP_USERNAME'), config('MFP_PASSWORD'))
+
+def all_empty(data):
+    for meal in data.values():
+        if len(meal) > 0:
+            return False
+    return True
+
+def fetch_streak_upto(when, accumulator={}, countdown=None):
+    while True:
+        data = client.get_date(when.year, when.month, when.day).get_as_dict()
+        if all_empty(data):
+            break
+        accumulator[when.date()] = data
+        if countdown:
+            countdown -= 1
+            if countdown == 0:
+                break
+        when = when - datetime.timedelta(days=1)
+        time.sleep(random.randint(1, 5))
+    return accumulator
+
+streak = fetch_streak_upto(datetime.datetime.today(), countdown=7)
+
+print(json.dumps({d.isoformat(): v for d,v in streak.items()}))
