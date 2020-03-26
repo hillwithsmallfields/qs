e3fb0f9db9ea10b5aff8f05389b3b6aa4bb7469b
John Sturdy <jcg.sturdy@gmail.com>
2020-03-26 21:52:02 +0000

    Start adding CSV i/o

--- qs/mfp_reader.py
+++ qs/mfp_reader.py
@@ -2,6 +2,7 @@
 # program to scrape myfitnesspal, using https://github.com/coddingtonbear/python-myfitnesspal
 
 import argparse
+import csv
 from decouple import config
 import datetime
 import json
@@ -11,23 +12,33 @@ import time
 
 client = None
 
+meal_keys = ['breakfast', 'lunch', 'dinner', 'snacks']
+meal_numbers = {v:i for i,v in enumerate(meal_keys)}
+
 def all_empty(data):
     for meal in data.values():
         if len(meal) > 0:
             return False
     return True
 
-def fetch_streak_upto(when, accumulator={}, countdown=None):
+def fetch_streak_upto(when,
+                      accumulator={}, sheet={},
+                      countdown=None):
     global client
     if client is None:
         client = myfitnesspal.Client(config('MFP_USERNAME'), config('MFP_PASSWORD'))
     while True:
         date_key = when.date()
         if date_key not in accumulator:
-            data = client.get_date(when.year, when.month, when.day).get_as_dict()
-            if all_empty(data):
+            day_data = client.get_date(when.year, when.month, when.day)
+            dict_data = day_data.get_as_dict()
+            if all_empty(dict_data):
                 break
-            accumulator[date_key] = data
+            accumulator[date_key] = dict_data
+            row = {mealname+'_cals': day_data.meals[i]['calories'] for i, mealname in meal_keys}
+            row.update(day_data.totals)
+            row['date' = date_key]
+            sheet[date_key] = row
             if countdown:
                 countdown -= 1
                 if countdown == 0:
@@ -42,44 +53,53 @@ def main():
                         type=int, default=0,
                         help="""Maximum number of days to fetch.""")
     parser.add_argument("--update", "-u",
-                       help="""File to update.""")
+                        action='store_true',
+                       help="""Read the existing file contents and add to them.""")
     parser.add_argument("--latest", "-l",
                         help="""The most recent date to fetch.""")
-    parser.add_argument("--preload", "-p")
-    parser.add_argument("--output", "-o")
+    parser.add_argument("--sheet", "-p")
+    parser.add_argument("--json", "-o")
     args = parser.parse_args()
 
-    if args.update:
-        if args.preload or args.output:
-            print("If --update is used, --preload and --output must not be used")
-            return 1
-        input_filename = args.update
-        output_filename = args.update
-    else:
-        if args.update:
-            print("If --output or --preload is used, --update must not be used.")
-        input_filename = args.preload
-        output_filename = args.output or "/tmp/mfp.json"
-
     so_far = {}
-    if input_filename:
-        with open(input_filename) as instream:
+    if args.update and args.json:
+        with open(args.json) as instream:
             so_far = {datetime.datetime.fromisoformat(k): v
                       for k, v in json.load(instream).items()}
+
+    rows = {}
+    if args.update and args.sheet:
+        with open(args.sheet) as instream:
+            rows = {row['date']: row
+                     for row in csv.DictReader(instream)}
 
     upto_date = (datetime.datetime.fromisoformat(args.latest)
                  if args.latest
                  else datetime.datetime.today())
 
-    with open(output_filename, 'w') as outstream:
-        json.dump({d.isoformat(): v
-                   for d,v in fetch_streak_upto(
-                           upto_date,
-                           accumulator=so_far,
-                           countdown=(args.number
+    fetch_streak_upto(upto_date,
+                      accumulator=so_far,
+                      sheet=rows,
+                      countdown=(args.number
                                       if args.number > 0
-                                      else None)).items()},
-                  outstream)
+                                      else None))
+
+    if args.json:
+        with open(args.json, 'w') as outstream:
+            json.dump({d.isoformat(): v
+                       for d,v in so_far.items()},
+                      outstream)
+
+    if args.sheet:
+        with open(args.sheet, 'w') as outstream:
+            fieldnames = ['date',
+                          'breakfast', 'lunch', 'dinner', 'snacks',
+                          'calories',
+                          'carbohydrates', 'fat', 'protein', 'sodium', 'sugar']
+            writer = csv.DictWriter(outstream, fieldnames=fieldnames)
+            writer.writeheader()
+            for row in rows:
+                writer.writerow(row)
 
 if __name__ == "__main__":
     main()
