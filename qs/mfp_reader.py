ced91ec0bc87edbfdc97422063cc6d7e6d9fdf17
John Sturdy <jcg.sturdy@gmail.com>
2020-04-23 20:55:02 +0100

    checkpoint on general work on MFP reader

--- qs/mfp_reader.py
+++ qs/mfp_reader.py
@@ -12,34 +12,56 @@ import time
 
 client = None
 
+def ensure_mfp_login():
+    """Ensure that we are connected to MyFitnessPal."""
+    global client
+    if client is None:
+        client = myfitnesspal.Client(config('MFP_USERNAME'), config('MFP_PASSWORD'))
+
 meal_keys = ['breakfast', 'lunch', 'dinner', 'snacks']
 meal_numbers = {v:i for i,v in enumerate(meal_keys)}
 
 def all_empty(data):
+    """Return whether all the meals in that data are empty."""
     for meal in data.values():
         if len(meal) > 0:
             return False
     return True
 
+def meal_calories(day_data, meal_index):
+    print("day_data", day_data, "meal_index", meal_index)
+    print("meals", day_data.meals)
+    print("this meal", day_data.meals[meal_index])
+    return day_data.meals[meal_index]['calories']
+
 def fetch_streak_upto(when,
                       accumulator, sheet,
                       countdown=None):
-    global client
-    if client is None:
-        client = myfitnesspal.Client(config('MFP_USERNAME'), config('MFP_PASSWORD'))
+    """Fetch a continuous series of days' data, from the specified date
+    going back until there is a break, or until a given number of days
+    have been fetched.
+
+    """
+    print("fetching with accumulator", accumulator, "and sheet", sheet)
+    ensure_mfp_login()
     while True:
         date_key = when.date()
         if date_key not in accumulator:
+            print("getting data for", when)
             day_data = client.get_date(when.year, when.month, when.day)
             dict_data = day_data.get_as_dict()
+            print("day_data is", day_data, "and dict_data is", dict_data)
             if all_empty(dict_data):
                 break
-            if accumulator:
+            if accumulator is not None:
+                print("adding", dict_data, "to json")
                 accumulator[date_key] = dict_data
-            if sheet:
-                row = {mealname+'_cals': day_data.meals[i]['calories'] for i, mealname in meal_keys}
+            if sheet is not None:
+                row = {mealname+'_cals': meal_calories(day_data, i)
+                       for i, mealname in enumerate(meal_keys)}
                 row.update(day_data.totals)
-                row['date' = date_key]
+                row['date'] = date_key
+                print("adding", row, "to sheet")
                 sheet[date_key] = row
             if countdown:
                 countdown -= 1
@@ -62,6 +84,10 @@ def main():
     parser.add_argument("--sheet", "-p")
     parser.add_argument("--json", "-o")
     args = parser.parse_args()
+
+    if args.sheet is None and args.json is None:
+        print("No output specified!  Use --sheet or --json or both.")
+        return 1
 
     so_far = {}
     if args.update and args.json:
