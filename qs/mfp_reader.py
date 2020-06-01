f0e6df3da1a748142cde7631b16c0e339363c032
John Sturdy <jcg.sturdy@gmail.com>
2020-06-01 20:59:09 +0100

    More debugging on mfp reader

--- qs/mfp_reader.py
+++ qs/mfp_reader.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 # program to scrape myfitnesspal, using https://github.com/coddingtonbear/python-myfitnesspal
 
+import sys
+import os
+sys.path.append(os.path.expanduser("~/open-projects/github.com/hillwithsmallfields/python-myfitnesspal/myfitnesspal"))
+
+print("path is now", sys.path)
+
 import argparse
 import csv
 from decouple import config
@@ -32,44 +38,71 @@ def meal_calories(day_data, meal_index):
     print("day_data", day_data, "meal_index", meal_index)
     print("meals", day_data.meals)
     print("this meal", day_data.meals[meal_index])
-    return day_data.meals[meal_index]['calories']
+    tot = day_data.meals[meal_index].totals
+    print("got totals", tot)
+    return tot.get('calories', 0)
 
 def fetch_streak_upto(when,
                       accumulator, sheet,
-                      countdown=None):
+                      countdown=None,
+                      verbose=False):
     """Fetch a continuous series of days' data, from the specified date
     going back until there is a break, or until a given number of days
     have been fetched.
 
     """
-    print("fetching with accumulator", accumulator, "and sheet", sheet)
+    if verbose:
+        print("fetching with accumulator", accumulator, "and sheet", sheet, "and countdown", countdown)
     ensure_mfp_login()
+    if verbose:
+        print("now logged in")
     while True:
         date_key = when.date()
-        if date_key not in accumulator:
-            print("getting data for", when)
+        print("on date_key", date_key, "for", when)
+        if ((accumulator is not None and date_key not in accumulator)
+            or (sheet is not None and date_key not in sheet)):
+            if verbose:
+                print("getting data for", when)
             day_data = client.get_date(when.year, when.month, when.day)
             dict_data = day_data.get_as_dict()
-            print("day_data is", day_data, "and dict_data is", dict_data)
+            if verbose:
+                print("day_data is", day_data, "and dict_data is", dict_data)
             if all_empty(dict_data):
+                if verbose:
+                    print("couldn't get any data for", date_key)
                 break
             if accumulator is not None:
-                print("adding", dict_data, "to json")
+                if verbose:
+                    print("adding", dict_data, "to json")
                 accumulator[date_key] = dict_data
             if sheet is not None:
+                if verbose:
+                    print("accumulating spreadsheet data")
                 row = {mealname+'_cals': meal_calories(day_data, i)
                        for i, mealname in enumerate(meal_keys)}
                 row.update(day_data.totals)
                 row['date'] = date_key
-                print("adding", row, "to sheet")
+                if verbose:
+                    print("adding", row, "to sheet with date", date_key)
                 sheet[date_key] = row
+            if verbose:
+                print("countdown was", countdown)
             if countdown:
                 countdown -= 1
-                if countdown == 0:
+                if verbose:
+                    print("countdown is", countdown)
+                if countdown <= 0:
                     break
         when = when - datetime.timedelta(days=1)
-        time.sleep(random.randint(1, 5))
+        pause = random.randint(1, 5)
+        if verbose:
+            print("pausing", pause, "before fetching previous day")
+        time.sleep(pause)
     return accumulator
+
+def find_last_unfetched_date(dict_by_date):
+    """Return the latest day before the first that appears as a key of a dict."""
+    return nil                  # todo: fill this in
 
 def main():
     parser = argparse.ArgumentParser()
@@ -81,8 +114,14 @@ def main():
                        help="""Read the existing file contents and add to them.""")
     parser.add_argument("--latest", "-l",
                         help="""The most recent date to fetch.""")
+    parser.add_argument("--autodate", "-a",
+                        action='store_true',
+                        help="""Work out how far back to start automatically (with --update only).""")
     parser.add_argument("--sheet", "-p")
     parser.add_argument("--json", "-o")
+    parser.add_argument("--verbose", "-v",
+                        action='store_true',
+                        help="""Produce some diagnostic output.""")
     args = parser.parse_args()
 
     if args.sheet is None and args.json is None:
@@ -103,14 +142,17 @@ def main():
 
     upto_date = (datetime.datetime.fromisoformat(args.latest)
                  if args.latest
-                 else datetime.datetime.today())
+                 else (find_last_unfetched_date(rows or so_far)
+                       if (args.autodate and args.update)
+                       else datetime.datetime.today()))
 
     fetch_streak_upto(upto_date,
                       accumulator=so_far if args.json else None,
                       sheet=rows if args.sheet else None,
                       countdown=(args.number
                                       if args.number > 0
-                                      else None))
+                                      else None),
+                      verbose=args.verbose)
 
     if args.json:
         with open(args.json, 'w') as outstream:
