065615ac10c6dc34d3bf9db8717cd1d2c8f28009
John Sturdy <jcg.sturdy@gmail.com>
2020-12-26 19:24:57 +0000

    Adjust scripts as I work towards tracking down certain transactions

--- qs/merge-latest-statement.lisp
+++ qs/merge-latest-statement.lisp
@@ -1,14 +1,24 @@
 (let* ((main (read-canonical "~/common/finances/finances.csv"))
-       (summarised (by-day main t))
+       (summarised (by-day main t t))
        (latest (read-canonical incoming-statement))
-       (unmatched (blank-sheet))
-       (regular-entries (blank-sheet))
-       (unambiguously-matched (blank-sheet))
-       (ambiguously-matched (blank-sheet)))
-  (print "Number of original entries:" (length main) " Number of summarised entries:" (length summarised))
+
+       (unmatched-automatic (blank-sheet))
+       (unmatched-non-automatic (blank-sheet))
+
+       (unambiguously-matched-auto (blank-sheet))
+       (unambiguously-matched-non-auto (blank-sheet))
+
+       (ambiguously-matched-auto (blank-sheet))
+       (ambiguously-matched-non-auto (blank-sheet)))
+  (print "Number of original entries:" (length main))
+  (print "Number of summarised entries:" (length summarised))
   (for-each-row latest this-row nil
                 (let* ((amount (get this-row "amount"))
-                       (matches (find-amount summarised amount (get this-row "timestamp") 7)))
+                       (direct-matches (find-amount main amount (get this-row "timestamp") 7))
+                       (summarised-matches (find-amount summarised amount (get this-row "timestamp") 7))
+                       (matches (append direct-matches summarised-matches))
+                       (payee (get this-row "payee"))
+                       (automatic (flagged-as "handelsbanken" payee "automatic")))
                   (if (> (length matches) 0)
                       (let ((found (find-by-field (get this-row "payee") matches "payee")))
                         (when verbose
@@ -20,26 +30,46 @@
                             (print "    payee" (get match "payee")
                                    "on" (get match "date"))))
                         (if (> (length matches) 1)
-                            (add-row ambiguously-matched this-row)
-                          (add-row unambiguously-matched this-row)))
-                    (let* ((payee (get this-row "payee"))
-                           (regular (flagged-as "handelsbanken" payee "regular")))
-                      (when verbose
-                        (print "no matches on amount" amount
-                               "for payee" payee
-                               "regular" regular
-                               "on" (get this-row "date")))
-                      (if regular
-                          (add-row regular-entries this-row)
-                        (when verbose
-                          (print "unmatched" unmatched "this-row" this-row))
-                        (add-row unmatched this-row))))
-                  ))
-  (print (length unmatched) "non-regular unmatched transactions")
-  (write-csv unmatched "unmatched-irregular.csv")
-  (print (length regular-entries) "regular unmatched transactions")
-  (write-csv regular-entries "unmatched-regular.csv")
-  (print (length ambiguously-matched) "ambiguously matched transactions")
-  (write-csv ambiguously-matched "ambiguously-matched.csv")
-  (print (length unambiguously-matched) "unambiguously matched transactions")
-  (write-csv unambiguously-matched "unambiguously-matched.csv"))
+                            (if automatic
+                                (add-row ambiguously-matched-auto this-row)
+                              (add-row ambiguously-matched-non-auto this-row))
+                          (if automatic
+                              (add-row unambiguously-matched-auto this-row)
+                            (add-row unambiguously-matched-non-auto this-row))))
+                    (when verbose
+                      (print "no matches on amount" amount
+                             "for payee" payee
+                             "automatic" automatic
+                             "on" (get this-row "date")))
+                    (if automatic
+                        (add-row unmatched-automatic this-row)
+                      (add-row unmatched-non-automatic this-row)))))
+  (let* ((parentage-table (read-parentage-table "cats.yaml"))
+         (classifiers (read-classifier "default-classes.yaml"))
+         (by-classification (by-month unmatched-automatic nil nil) parentage-table classifiers t)         
+         (handled-count (+ (length unmatched-automatic)
+                           (length unmatched-non-automatic)
+                           (length ambiguously-matched-auto)
+                           (length ambiguously-matched-non-auto)
+                           (length unambiguously-matched-auto)
+                           (length unambiguously-matched-non-auto))))
+    (print "incoming transactions:" (length latest))
+    (print "unmatched automatic transactions:" (length unmatched-automatic))
+    (print "unmatched non-automatic transactions:" (length unmatched-non-automatic))
+    (print "ambiguously matched automatic transactions:" (length ambiguously-matched-auto))
+    (print "ambiguously matched non-automatic transactions:" (length ambiguously-matched-non-auto))
+    (print "unambiguously matched automatic transactions:" (length unambiguously-matched-auto))
+    (print "unambiguously matched non-automatic transactions:" (length unambiguously-matched-non-auto))
+    (print "total transactions handled:" handled-count)
+    (print "fallen through the gaps:" (- (length latest) handled-count))
+
+    (write-csv unmatched-automatic "unmatched-auto.csv")
+    (write-csv unmatched-non-automatic "unmatched-non-auto.csv")
+
+    (write-csv ambiguously-matched-auto "ambiguously-matched-auto.csv")
+    (write-csv ambiguously-matched-non-auto "ambiguously-matched-non-auto.csv")
+
+    (write-csv unambiguously-matched-auto "unambiguously-matched-auto.csv")
+    (write-csv unambiguously-matched-non-auto "unambiguously-matched-non-auto.csv")
+
+    (write-csv by-classification "automatics-by-classification.csv")))
