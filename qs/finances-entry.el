de5b7e4eb6e5f48c43242cf816d345766435a164
John Sturdy <jcg.sturdy@gmail.com>
2021-01-30 20:57:04 +0000

    Add command for entering transfers

--- qs/finances-entry.el
+++ qs/finances-entry.el
@@ -74,7 +74,22 @@ With optional FORCE, do it even if it seems unnecessary."
     (format "%g" cell-value))
    (t cell-value)))
 
-(defun finances-enter (date time account amount currency original-amount original-currency category payee project note)
+(defun adjust-date-time (date time)
+  "Adjust the given DATE and TIME to avoid clashing with any already in the buffer."
+  (while (save-excursion
+           (goto-char (point-min))
+           (search-forward (format "%s,%s," date time) (point-max) t))
+    (let ((date-time (time-add (parse-iso8601-time-string (format "%sT%s" date time))
+                               1)))
+      (setq date (format-time-string "%F" date-time)
+            time (format-time-string "%T" date-time))))
+  (cons date time))
+
+(defun finances-enter (date time
+                            account amount currency
+                            original-amount original-currency
+                            category payee
+                            project note)
   "Make an entry for my finances file.
 The fields DATE TIME ACCOUNT AMOUNT CURRENCY ORIGINAL-AMOUNT
 ORIGINAL-CURRENCY CATEGORY PARENT PAYEE LOCATION PROJECT NOTE are
@@ -85,9 +100,12 @@ as in the Financisto app."
     (goto-char (point-max))
     (unless (looking-at "^$")
       (insert "\n"))
-    (let ((balance nil))
+    (let* ((balance nil)
+           (adjusted-date-time (adjust-date-time date time))
+           (adjusted-date (car adjusted-date-time))
+           (adjusted-time (cdr adjusted-date-time)))
       (insert (mapconcat 'finances-entry-cell-as-string
-                         (list date time
+                         (list adjusted-date adjusted-time
                                account amount currency
                                original-amount original-currency
                                balance
@@ -96,3 +114,48 @@ as in the Financisto app."
                                note)
                          ",")
               "\n"))))
+
+(defun finances-read-transfer ()
+  "Read the values needed to record a fund transfer."
+  (finances-read-completions)
+  (let* ((completion-ignore-case t)
+         (date (format-time-string "%F"))
+         (time (format-time-string "%T"))
+         (from-amount (read (read-from-minibuffer "Amount in source currency: ")))
+         (from-currency (completing-read "From currency: "
+                                         currency-completions
+                                         nil t
+                                         finances-default-currency))
+         (from-account (completing-read "From account: "
+                                        account-completions
+                                        nil t
+                                        finances-default-account))
+         (to-currency (completing-read "To currency: "
+                                       currency-completions
+                                       nil t
+                                       finances-default-currency))
+         (to-amount (if (string= from-currency to-currency)
+                        from-amount
+                      (read (read-from-minibuffer "Amount in destination currency: "))))
+         (to-account (completing-read "To account: "
+                                      account-completions
+                                      nil t
+                                      finances-default-account)))
+    (list date time
+          from-amount from-currency from-account
+          to-amount to-currency to-account)))
+
+(defun finances-transfer (date time from-amount from-currency from-account to-amount to-currency to-account)
+  "Make a pair of transfer entries in my finances file.
+Argument DATE is the date of the transaction; TIME the time of it.
+FROM-AMOUNT is how much was transferred in FROM-CURRENCY
+and TO-AMOUNT is how much it became in TO-CURRENCY.
+The transfer is from FROM-ACCOUNT to TO-ACCOUNT."
+  (interactive (finances-read-transfer))
+  (finances-enter date time
+                  from-account from-amount from-currency
+                  from-amount from-currency
+                  "Transfer" to-account "" "")
+  (finances-enter date time
+                  to-account to-amount from-currency
+                  from-amount from-currency "Transfer" from-account "" ""))
