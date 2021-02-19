0bc0a6529fe3c5582b7be68075ac5987787f5339
John Sturdy <jcg.sturdy@gmail.com>
2021-02-19 21:43:42 +0000

    Get it finding all the message types I want, and calling handlers (mostly stubs)

--- qs/fetch_ebay_mails.py
+++ qs/fetch_ebay_mails.py
@@ -9,14 +9,51 @@ import email.parser
 import email.policy
 import getpass
 import imaplib
+import os
 import traceback
 import re
 
 import named_column_sheet
 
-def process_plain_text(payload, result, message_date, number):
-    with open("/tmp/ebay/msg-%s.txt" % number, 'w') as outstream:
+def start_of(s):
+    return "|".join(str(s).strip().split('\r\n')).replace('-', '')[:244]
+    # return str(s)
+    # return "|".join(str(s).split())
+
+def process_we_got_your_order(payload, result, message_date, number):
+    print("     WGYO", start_of(payload))
+    with open("/tmp/ebay/msg-wgyo-%s.html" % number, 'w') as outstream:
         outstream.write(payload)
+
+    # look for <a href="https://rover.ebay.com/rover/...> for the item names
+        
+    count = 0
+    seller = None
+    item_name = None
+    price = None
+    p_and_p_price = None
+    quantity = None
+    item_total = None
+    return count
+
+def process_order_confirmed(payload, result, message_date, number):
+    print("     OC", start_of(payload))
+    with open("/tmp/ebay/msg-oc-%s.html" % number, 'w') as outstream:
+        outstream.write(payload)
+    count = 0
+    seller = None
+    item_name = None
+    price = None
+    p_and_p_price = None
+    quantity = None
+    item_total = None
+    return count
+
+def process_confirmation_of_your_order(payload, result, message_date, number):
+    print("     COYO", start_of(payload))
+    with open("/tmp/ebay/msg-coyo-%s.txt" % number, 'w') as outstream:
+        outstream.write(payload)
+    count = 0
     seller = None
     item_name = None
     price = None
@@ -69,8 +106,17 @@ def process_plain_text(payload, result, message_date, number):
                     'price': price,
                     'p_and_p_price': p_and_p_price,
                     'quantity': quantity,
-                    'item_total': item_total})
+                    'item_total': item_total,
+                    'message_number': number})
                 item_name = None
+                count += 1
+    return count
+
+handlers = {
+    "Confirmation of your order": ('text/plain', process_confirmation_of_your_order),
+    "Order confirmed": ('text/html', process_order_confirmed),
+    "Order Confirmed": ('text/html', process_order_confirmed),
+    "We got your order": ('text/html', process_we_got_your_order)}
 
 def process_order_confirmations(M):
     rv, data = M.search(None, "ALL")
@@ -85,27 +131,43 @@ def process_order_confirmations(M):
         'price',
         'p_and_p_price',
         'quantity',
-        'item_total'])
-    for message_number in str(data[0]).split(' '):
-        print("Getting message", message_number)
+        'item_total',
+        'message_number'])
+    message_list = data[0].decode('utf-8').split(' ')
+    print("fetching", len(message_list), "messages")
+    for message_number in message_list:
+        # print("Getting message", message_number)
         try:
             rv, message_data = M.fetch(message_number, '(RFC822)')
             if rv != 'OK':
                 print("Could not fetch message", message_number)
                 continue
+            acted = '    '
             message_contents = email.message_from_bytes(message_data[0][1], policy=email.policy.default)
             message_date = datetime.datetime.strptime(message_contents['Date'], "%a, %d %b %Y %H:%M:%S %z")
-            if message_contents['Subject'].startswith("Confirmation of your order"):
-                for part in message_contents.walk():
-                    if part.get_content_type() == 'text/plain':
-                        process_plain_text(part.get_content(), result, message_date, message_number)
-                        break
+            message_subject = message_contents['Subject']
+            message_from = message_contents['From']
+            if message_from == "eBay <ebay@ebay.co.uk>" or message_from == "eBay <ebay@ebay.com>":
+                counted = 0
+                for s in handlers.keys():
+                    if s in message_subject:
+                        handler = handlers[s]
+                        acted = 'm   '
+                        for part in message_contents.walk():
+                            if part.get_content_type() == handler[0]:
+                                counted = handler[1](part.get_content(), result, message_date, message_number)
+                                acted = 'p %02d' % counted 
+                                break
+                        if counted != 0:
+                            break
+            print(acted, "message %s: from %s on %s: %s" % (message_number, message_from, message_date, message_subject))
         except Exception as ex:
             print("error", ex)
             traceback.print_exc()
     result.write_csv("/tmp/orders.csv", suppress_timestamp=True)
 
 def main():
+    os.makedirs("/tmp/ebay", exist_ok=True)
     M = imaplib.IMAP4_SSL('imap.gmail.com')
     try:
         M.login(decouple.config('EBAY_EMAIL_ADDRESS'), getpass.getpass())
