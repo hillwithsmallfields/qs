a952ac6e31d36cc31c5d7f2927a78dbf26c08b8f
John Sturdy <jcg.sturdy@gmail.com>
2021-05-02 12:21:24 +0100

    Implement page sections systematically

--- qs/automatic.py
+++ qs/automatic.py
@@ -80,52 +80,88 @@ def age_string(person, year):
     age = contacts_data.age_in_year(person, year)
     return str(age) if age else "?"
 
-def construct_dashboard_page(config, charts_dir):
+def namify(x):
+    return x.replace(' ', '_')
+
+def make_name_with_email(name, email):
+    return (T.a(href="email:"+email)[name]
+            if email and email != ""
+            else name)
+
+def row(*things):
+    return T.table(width="100%")[T.tr[[T.td[thing] for thing in things]]]
+
+class SectionalPage(object):
+    pass
+
+    def __init__(self):
+        self._sections = []
+
+    def add_section(self, title, body):
+        self._sections.append((title, body))
+
+    def toc(self):
+        return [T.h2["Table of contents"],
+                T.ul[[T.li[T.a(href="#"+namify(section[0]))[section[0]]] for section in self._sections]]]
+
+    def sections(self):
+        return [[T.div(class_="section")[T.h2[T.a(name=namify(section[0]))[section[0]]],
+                       T.div(class_="sectionbody")[section[1]]] for section in self._sections]]
+
+def weight_section():
+    return T.img(src="weight-stone.png")
+
+def spending_section():
+    return T.div[T.p["Full details ", T.a(href="by-class.html")["here"], "."],
+                 T.img(src="by-class.png")]
+
+def budgetting_section(config, charts_dir):
     thresholds = classify.read_thresholds(config, "budgetting-thresholds.yaml")
-    print("thresholds are", thresholds)
     with open(os.path.join(charts_dir, "by-class-this-year.csv")) as spent_stream:
         spent = [row for row in csv.DictReader(spent_stream)]
-    spent_month_before_last = spent[-3] if len(spent) >= 3 else None
-    spent_last_month = spent[-2] if len(spent) >= 2 else None
-    spent_this_month = spent[-1]
-    people_by_id, _ = contacts_data.read_contacts(os.path.expandvars("$COMMON/org/contacts.csv"))
-    today = datetime.date.today()
-    this_year = today.year
-    birthday_people = sorted([person
-                              for person in people_by_id.values()
-                              if birthday_soon(person, this_year, today)],
-                             key=lambda person: birthday(person, this_year))
-    # TODO: construct in sections, with automatic building of TOC
-    return [T.body[
-        T.h1["My dashboard"],
-        T.h2["Weight"],
-        T.img(src="weight-stone.png"),
-        T.h2["Spending"],
-        T.p["Full details ", T.a(href="by-class.html")["here"], "."],
-        T.img(src="by-class.png"),
-        T.h3["Monthly budgets"],
-        T.table(class_='budgetting')[
+        spent_month_before_last = spent[-3] if len(spent) >= 3 else None
+        spent_last_month = spent[-2] if len(spent) >= 2 else None
+        spent_this_month = spent[-1]
+        return T.table(class_='budgetting')[
             T.tr[[T.th[""]] + [T.th[coi] for coi in CATEGORIES_OF_INTEREST]],
             T.tr(class_='monthly_budget')[[T.th["Monthly budget"]] + [T.td(class_='budget')[str(thresholds[coi])] for coi in CATEGORIES_OF_INTEREST]],
             (T.tr(class_='spent_month_before_last')[[T.th["Spent month before last"]] + [T.td(class_='spent')[str(spent_month_before_last[coi])] for coi in CATEGORIES_OF_INTEREST]]) if spent_month_before_last else [], # TODO: style according to whether over threshold
             (T.tr(class_='spent_last_month')[[T.th["Spent last month"]] + [T.td(class_='spent')[str(spent_last_month[coi])] for coi in CATEGORIES_OF_INTEREST]]) if spent_last_month else [], # TODO: style according to whether over threshold
             T.tr(class_='spent_this_month')[[T.th["Spent this month"]] + [T.td(class_='spent')[str(spent_this_month[coi])] for coi in CATEGORIES_OF_INTEREST]], # TODO: style according to whether over threshold
-            T.tr(class_='remaining_this_month')[[T.th["Remaining this month"]] + [make_remaining_cell(thresholds, spent_this_month, coi) for coi in CATEGORIES_OF_INTEREST]]],
-        T.h2["Upcoming birthdays"],
-        T.table(class_='birthdays')[
-            T.tr[T.th["Birthday"], T.th["Name"], T.th["Age"]],
-            [T.tr[T.td(class_='birthday')[str(birthday(person, this_year))],
-                  # TODO: put email address as link on name, if known
-                      T.td(class_='name')[contacts_data.make_name(person)],
-                  T.td(class_='age')[age_string(person, this_year)]]
-                 for person in birthday_people]],
-        T.h2["Food to use up in fridge"],
-        # TODO: perishables from Coimealta
-        # TODO: dietary data from MFP
-        # TODO: exercise data from MFP
-        # TODO: actions list from org-mode
-        # TODO: shopping list from org-mode
-    ]]
+            T.tr(class_='remaining_this_month')[[T.th["Remaining this month"]] + [make_remaining_cell(thresholds, spent_this_month, coi) for coi in CATEGORIES_OF_INTEREST]]]
+
+def birthdays_section():
+    people_by_id, _ = contacts_data.read_contacts(os.path.expandvars("$COMMON/org/contacts.csv"))
+    today = datetime.date.today()
+    this_year = today.year
+    return T.table(class_='birthdays')[
+        T.tr[T.th["Birthday"], T.th["Name"], T.th["Age"]],
+        [T.tr[T.td(class_='birthday')[str(birthday(person, this_year))],
+              # TODO: put email address as link on name, if known
+              T.td(class_='name')[make_name_with_email(contacts_data.make_name(person),
+                                                       person.get('Primary email', ""))],
+              T.td(class_='age')[age_string(person, this_year)]]
+         for person in sorted([person
+                              for person in people_by_id.values()
+                              if birthday_soon(person, this_year, today)],
+                              key=lambda person: birthday(person, this_year))]]
+
+def construct_dashboard_page(config, charts_dir):
+    page = SectionalPage()
+    page.add_section("Weight", weight_section())
+    page.add_section("Spending", spending_section())
+    page.add_section("Monthly budgets", budgetting_section(config, charts_dir))
+    page.add_section("Upcoming birthdays", birthdays_section())
+    page.add_section("Food to use up in fridge", T.p["placeholder"])
+    page.add_section("Diet", T.p["placeholder"])
+    page.add_section("Exercise", T.p["placeholder"])
+    page.add_section("Actions", T.p["placeholder"])
+    page.add_section("Shopping", T.p["placeholder"])
+    page.add_section("Texts", T.p["placeholder"])
+    return [T.body[
+        T.h1["My dashboard"],
+        row(page.toc(), "Text to right of TOC"),
+        page.sections()]]
 
 def page_text(page_contents, style_text, script_text):
     return untemplate.Serializer(untemplate.examples_vmap, 'utf-8').serialize(
@@ -261,6 +297,7 @@ def automatic_contacts(charts_dir, archive_dir):
         shutil.copy(contacts_scratch, contacts_file)
     else:
         print("wrong number of people after linking contacts, originally", original_lines, "but now", scratch_lines)
+    # todo: backup files generally
 
 def automatic_actions(charts_dir,
                       begin_date, end_date,
@@ -281,7 +318,11 @@ def automatic_actions(charts_dir,
     automatic_physical(charts_dir, begin_date, end_date, archive_dir)
     automatic_contacts(charts_dir, archive_dir)
     if do_externals:
-        mfp_reader.automatic(config, mfp_filename, verbose)
+        if os.path.getmtime(mfp_filename) + datetime.timedelta(hours=23, minutes=30) < datetime.datetime.now():
+            print("Fetching data from myfitnesspal.com")
+            mfp_reader.automatic(config, mfp_filename, verbose)
+        else:
+            print("myfitnesspal.com data fetched within the past day or so, so not doing again yet")
 
     write_dashboard_page(config, charts_dir)
 
