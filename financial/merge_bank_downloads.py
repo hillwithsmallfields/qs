c89e597bec7f057ebc7ed5b07ca10ac9a50b3a9a
John C. G. Sturdy <jcg.sturdy@gmail.com>
2023-01-01 12:10:18 +0100

    Add more command line args

--- financial/merge_bank_downloads.py
+++ financial/merge_bank_downloads.py
@@ -8,15 +8,10 @@ from frozendict import frozendict
 
 import finutils
 
-BANK_BASE = os.path.expanduser("~/Sync/finances/handelsbanken/handelsbanken-base.csv")
-BANK_FULL = os.path.expanduser("~/Sync/finances/handelsbanken/handelsbanken-full.csv")
-BANK_COLUMNS = ('Date', 'Details', 'Money out', 'Money in', 'Balance')
-UPDATES_GLOB = os.path.expanduser("~/Downloads/Transactions*.csv")
-
 def merge_bank_downloads(base, account, output):
     bank_full = finutils.read_csv(base)
     bank_full_ts = os.path.getmtime(base)
-    for download in glob.glob(UPDATES_GLOB):
+    for download in glob.glob(finutils.UPDATES_GLOB):
         if os.path.getmtime(download) > bank_full_ts:
             bank_full |= {
                 frozendict({
@@ -29,13 +24,13 @@ def merge_bank_downloads(base, account, output):
                 for raw in finutils.read_csv(download)
                 if (raw['Narrative'] != "Opening Balance "
                     and int(raw['Account']) == account)}
-    finutils.write_csv(bank_full, BANK_COLUMNS, output)
+    finutils.write_csv(bank_full, finutils.BANK_COLUMNS, output, lambda r: (r['Date'], r'[Details'))
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--account", "-a", type=int)
-    parser.add_argument("--base", "-b", default=BANK_BASE)
-    parser.add_argument("--output", "-o", default=BANK_FULL)
+    parser.add_argument("--base", "-b", default=finutils.BANK_BASE)
+    parser.add_argument("--output", "-o", default=finutils.BANK_FULL)
     return vars(parser.parse_args())
 
 if __name__ == "__main__":
