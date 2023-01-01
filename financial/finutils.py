3911540c380f1005bfbc0867c1907efe03917cdf
John C. G. Sturdy <jcg.sturdy@gmail.com>
2023-01-01 12:09:48 +0100

    put filename constants in here
    And pass a sort key in to the writer function

--- financial/finutils.py
+++ financial/finutils.py
@@ -1,16 +1,30 @@
 import csv
 import os.path
+import yaml
 from frozendict import frozendict
+
+MAIN_ACCOUNTING_FILE = "~/Sync/finances/finances.csv"
+MAIN_HEADERS = ('date', 'time', 'account', 'amount', 'currency',
+                'original_amount', 'original_currency', 'balance', 'statement',
+                'payee', 'category', 'project', 'item', 'message', 'combicount')
+
+BANK_BASE = "~/Sync/finances/handelsbanken/handelsbanken-base.csv"
+BANK_FULL = "~/Sync/finances/handelsbanken/handelsbanken-full.csv"
+BANK_COLUMNS = ('Date', 'Details', 'Money out', 'Money in', 'Balance')
+
+UPDATES_GLOB = "~/Downloads/Transactions*.csv"
+
+CONVERSIONS = "~/Sync/finances/conversions.yaml"
 
 def read_csv(filename):
     with open(os.path.expanduser(filename)) as instream:
         return set(frozendict(row) for row in csv.DictReader(instream))
 
-def write_csv(data, header, filename):
+def write_csv(data, header, filename, sort_key):
     with open(os.path.expanduser(filename), 'w') as outstream:
         writer = csv.DictWriter(outstream, fieldnames=header)
         writer.writeheader()
-        for row in sorted(data, key=lambda r: (r['Date'], r'[Details')):
+        for row in sorted(data, key=sort_key):
             writer.writerow(row)
 
 def read_yaml(filename):
@@ -18,5 +32,6 @@ def read_yaml(filename):
         return yaml.safe_load(instream)
 
 def show_sample(table, n_samples=12):
+    table = sorted(table, key=lambda r: (r.get('Date', r.get('date')), r'[Details'))
     for i in range(0, len(table), len(table)//n_samples):
         print(i, table[i])
