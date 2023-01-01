dae4bcc8be40025303f1849d45d73d694383efb7
John C. G. Sturdy <jcg.sturdy@gmail.com>
2023-01-01 12:10:56 +0100

    use sets; write output; add command line args

--- financial/third_attempt.py
+++ financial/third_attempt.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
+import argparse
 import csv
 import os.path
 import re
-import yaml
 
 from frozendict import frozendict
 
@@ -16,29 +16,38 @@ def without_numeric_tail(string):
 def convert_bank_row(row, conversions):
     trimmed = without_numeric_tail(row['Details'])
     conversion = conversions.get(trimmed, {})
+    amount = float(row.get('Money in') or 0) - float(row.get('Money out') or 0)
     return frozendict({
         'account': "Handelsbanken current account",
         'date': row.get('Date'),
+        'time': "00:00:01",
         'payee': conversion.get('payee', trimmed),
         'category': conversion.get('category', 'unknown category'),
-        'amount': float(row.get('Money in') or 0) - float(row.get('Money out') or 0),
+        'amount': amount,
+        'currency': 'GBP',
+        'original_amount': amount,
+        'original_currency': 'GBP',
         'statement': float(row.get('Balance', 0)),
     })
 
 def convert_bank_table(table, conversions):
     return set(convert_bank_row(row, conversions) for row in table)
 
-def finances_update():
-    conversions = finutils.read_yaml("~/Sync/finances/conversions.yaml")['formats']['Default']['conversions']
-    main = finutils.read_csv("~/Sync/finances/finances.csv")
-    bank = finutils.read_csv("~/Sync/finances/handelsbanken/handelsbanken-full.csv")
-    converted = convert_bank_table(bank, conversions)
-    print("main")
-    finutils.show_sample(main)
-    print("bank")
-    finutils.show_sample(bank)
-    print("converted")
-    finutils.show_sample(converted)
+def finances_update(base, incoming, output, conversions):
+    finutils.write_csv(finutils.read_csv(base)
+                       | convert_bank_table(finutils.read_csv(incoming),
+                                            finutils.read_yaml(conversions)['formats']['Default']['conversions']),
+                       finutils.MAIN_HEADERS,
+                       output,
+                       lambda r: (r['date'], r['time'], r'[payee'))
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--base", "-b", default=finutils.MAIN_ACCOUNTING_FILE)
+    parser.add_argument("--incoming", "-i", default=finutils.BANK_BASE)
+    parser.add_argument("--output", "-o", default=finutils.MAIN_ACCOUNTING_FILE)
+    parser.add_argument("--conversions", "-c", default=finutils.CONVERSIONS)
+    return vars(parser.parse_args())
 
 if __name__ == "__main__":
-    finances_update()
+    finances_update(**get_args())
