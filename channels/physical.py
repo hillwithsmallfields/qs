67c42cf439f870fd860f2dc26019bb757743e2ec
John C. G. Sturdy <jcg.sturdy@gmail.com>
2023-12-23 14:05:33 +0100

    checkpoint in reworking physical data handling

--- channels/physical.py
+++ channels/physical.py
@@ -1,30 +1,77 @@
+from collections import defaultdict
 import datetime
 import os
 import shutil
 import sys
 
 import channels.panels as panels
+import dobishem
+from expressionive.expressionive import htmltags as T
+import expressionive.expridioms
 
-def ensure_in_path(directory):
-    if directory not in sys.path:
-        sys.path.append(directory)
+def merge_garmin_downloads(downloads):
+    """Merge the downloads.
+    They will come in file modification order, and we want to keep
+    only the latest if the files overlap.
+    """
+    activities_by_timestamp = dict()
+    for download in downloads:
+        for activity in download:
+            activities_by_timestamp[activity['Date']] = activity
+    return [
+        activities_by_timestamp[when]
+        for when in sorted(activities_by_timestamp.keys())
+    ]
 
-source_dir = os.path.dirname(os.path.realpath(__file__))
+def convert_existing(raw):
+    # TODO: convert duration strings to datetime.duration
+    return raw
 
-# This corresponds to https://github.com/hillwithsmallfields
-my_projects = os.path.dirname(os.path.dirname(source_dir))
+def convert_weight(raw):
+    return {}
 
-ensure_in_path(os.path.dirname(source_dir))
+def convert_garmin(raw):
+    # TODO: convert duration strings to datetime.duration
+    return {}
 
-import backup
+def convert_isometric(raw):
+    # TODO: convert duration strings to datetime.duration
+    return {}
 
-import qsutils.qsutils            # https://github.com/hillwithsmallfields/qs/blob/master/utils/qsutils.py
-from expressionive.expridioms import switchable_panel
+def combine_physical_data(incoming_lists):
+    by_date = defaultdict(dict)
+    for list_in in incoming_lists:
+        for entry in list_in:
+            existing = by_date[entry['Date']]
+            match entry.get('Activity Type'):
+                case 'Cycling':
+                    for aspect in ['Cycling distance', 'Cycling elapsed time', 'Cycle moving time']:
+                        if (got := existing.get(aspect)):
+                            existing[aspect] += entry[aspect]
+                        pass
+	            'Cycling max speed'	'Cycling average speed'
+
+                    pass
+                case 'Running' | 'Trail Running'::
+                    pass
+                case 'Walking':
+                    pass
+                case 'Open Water Swimming':
+                    pass
+                case 'Other':
+                    if entry.get('Title', "").startswith("Elliptical Trainer"):
+                        pass
+                case _:
+                    pass
+    # TODO: stringify everything for writing
 
 class PhysicalPanel(panels.DashboardPanel):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args)
+        self.accumulated_garmin_downloads_filename = os.path.expandvars("$SYNCED/health/garmin-downloads.csv")
+        self.combined_physical_data_filename = os.path.expandvars("$SYNCED/health/physical.csv")
+        self.physical_data = None
         self.updated = None
 
     def name(self):
@@ -33,26 +80,27 @@ class PhysicalPanel(panels.DashboardPanel):
     def label(self):
         return 'Health'
 
+    def fetch(self):
+        """Fetch Garmin downloads and merge them into an accumulated file."""
+        dobishem.storage.combined(
+            self.accumulated_garmin_downloads_filename,
+            merge_garmin_downloads,
+            {filename: process_raw_garmin_file
+             for filename in dobishem.in_modification_order("~/Downloads/Activities*.csv")})
+
     def update(self, **kwargs):
 
         """Merge incoming health-related data from various files, into one central file."""
 
-        physical = os.path.expandvars("$SYNCED/health/physical.csv")
-        phys_scratch = "/tmp/physical-tmp.csv"
-
-        physical_files = [os.path.expandvars("$SYNCED/health/weight.csv")
-                         # TODO: merge the other physical files
-                        ]
-
-        by_date = qsutils.qsmerge.qsmerge(physical,
-                                          physical_files, None, phys_scratch)
-
-        if qsutils.check_merged_row_dates.check_merged_row_dates(phys_scratch, physical, *physical_files):
-            backup.backup(physical, self.facto.file_config('backups', 'archive'), "physical-to-%s.csv")
-            shutil.copy(phys_scratch, physical)
-        else:
-            if verbose:
-                print("merge of physical data produced the wrong number of rows")
+        self.physical_data = dobishem.storage.combined(
+            self.combined_physical_data_filename,
+            combine_physical_data,
+            {
+                self.combined_physical_data_filename: convert_existing,
+                os.path.expandvars("$SYNCED/health/weight.csv"): convert_weight,
+                self.accumulated_garmin_downloads_filename: convert_garmin,
+                os.path.expandvars("$SYNCED/health/isometric.csv"): convert_isometric,
+            })
 
         self.updated = datetime.datetime.now()
         return self
