a6d368bc56b5a6715a96f4db6f00bdc75078dee4
John C. G. Sturdy <jcg.sturdy@gmail.com>
2023-12-23 18:24:44 +0100

    checkpoint before first run of new physical code

--- channels/physical.py
+++ channels/physical.py
@@ -6,6 +6,7 @@ import sys
 
 import channels.panels as panels
 import dobishem
+from dobishem.nested_messages import BeginAndEndMessages
 from expressionive.expressionive import htmltags as T
 import expressionive.expridioms
 
@@ -17,6 +18,7 @@ def parse_duration(dur):
 
 CELL_CONVERSIONS = {
     'Activity Type': str,
+    'Avg HR': float,
     'Avg Stroke Rate': float,
     'Avg. Swolf': float,
     'Calories': float,
@@ -40,6 +42,7 @@ CELL_CONVERSIONS = {
     'Left forearm': float,
     'Left thigh': float,
     'Left upper arm': float,
+    'Max HR': float,
     'Moving Time': parse_duration,
     'Peak flow (a.m.)': int,
     'Peak flow (p.m.)': int,
@@ -51,6 +54,7 @@ CELL_CONVERSIONS = {
     'Right forearm': float,
     'Right thigh': float,
     'Right upper arm': float,
+    'Run distance': float,
     'Run elapsed time': parse_duration,
     'Run moving time': parse_duration,
     'Stone': int,
@@ -61,6 +65,9 @@ CELL_CONVERSIONS = {
     'Time': parse_duration,
     'Total Strokes': int,
     'Waist': float,
+    'Walk distance': float,
+    'Walk elapsed time': parse_duration,
+    'Walk moving time': parse_duration,
 }
 
 def filter_conversions(*just_these):
@@ -76,23 +83,29 @@ def apply_conversions(raw, conversions):
             if field in raw}
 
 EXERCISE_CONVERSIONS = filter_conversions(
-            'Date',
-            'Date number',
-            'Calories at gym',
-            'Swim distance',
-            'Swim time',
-            'Plank longest',
-            'Plank total',
-            'Cycle distance',
-            'Cycle elapsed time',
-            'Cycle moving time',
-            'Cycle max speed',
-            'Cycle average speed',
-            'Run distance',
-            'Run elapsed time',
-            'Run moving time',
-            'Run average speed',
-            'Comment')
+    'Avg HR',
+    'Calories at gym',
+    'Cycle average speed',
+    'Cycle distance',
+    'Cycle elapsed time',
+    'Cycle max speed',
+    'Cycle moving time',
+    'Date number',
+    'Date',
+    'Max HR',
+    'Plank longest',
+    'Plank total',
+    'Run average speed',
+    'Run distance',
+    'Run elapsed time',
+    'Run moving time',
+    'Swim distance',
+    'Swim time',
+    'Walk average speed',
+    'Walk distance',
+    'Walk elapsed time',
+    'Walk moving time',
+    'Comment')
 
 MEASUREMENT_CONVERSIONS = filter_conversions(
     'Date',
@@ -124,18 +137,20 @@ MEASUREMENT_CONVERSIONS = filter_conversions(
     'Comment'    )
 
 GARMIN_CONVERSIONS = filter_conversions(
-            'Activity Type',
-            'Avg Stroke Rate',
-            'Avg. Swolf',
-            'Calories',
-            'Date',
-            'Distance',
-            'Elapsed Time',
-            'Moving Time',
-            'Swim distance',
-            'Swim time',
-            'Time',
-            'Total Strokes')
+    'Activity Type',
+    'Avg HR',
+    'Avg Stroke Rate',
+    'Avg. Swolf',
+    'Calories',
+    'Date',
+    'Distance',
+    'Elapsed Time',
+    'Max HR',
+    'Moving Time',
+    'Swim distance',
+    'Swim time',
+    'Time',
+    'Total Strokes')
 
 def safe_call(fn, arg):
     try:
@@ -149,13 +164,14 @@ def merge_garmin_downloads(downloads):
     only the latest if the files overlap.
     """
     activities_by_timestamp = dict()
-    for download in downloads:
-        for activity in download:
-            activities_by_timestamp[activity['Date']] = activity
-    return [
-        activities_by_timestamp[when]
-        for when in sorted(activities_by_timestamp.keys())
-    ]
+    with BeginAndEndMessages("Merging Garmin downloads"):
+        for download in downloads:
+            for activity in download:
+                activities_by_timestamp[activity['Date']] = activity
+        return [
+            activities_by_timestamp[when]
+            for when in sorted(activities_by_timestamp.keys())
+        ]
 
 def convert_exercise(raw):
     return apply_conversions(raw, EXERCISE_CONVERSIONS)
@@ -188,24 +204,46 @@ def convert_isometric(raw):
 
 def combine_exercise_data(incoming_lists):
     by_date = defaultdict(dict)
-    for list_in in incoming_lists:
-        for entry in list_in:
-            existing = by_date[entry['Date']]
-            match entry.get('Activity Type'):
-                case 'Cycling':
-                    pass
-                case 'Running' | 'Trail Running'::
-                    pass
-                case 'Walking':
-                    pass
-                case 'Open Water Swimming':
-                    pass
-                case 'Other':
-                    if entry.get('Title', "").startswith("Elliptical Trainer"):
+    with BeginAndEndMessages("Combining exercise data"):
+        for list_in in incoming_lists:
+            for entry in list_in:
+                existing = by_date[entry['Date']]
+                # existing['Avg HR'] # TODO: calculate this
+                existing['Max HR'] = max(existing.get('Max HR', 0), entry['Max HR'])
+                existing['Calories'] = existing.get('Calories', 0) + entry['Calories']
+                match entry.get('Activity Type'):
+                    case 'Cycling':
+                        existing['Cycle distance'] = existing.get('Cycle distance', 0) + entry['Distance']
+                        existing['Cycle moving time'] = existing.get('Cycle moving time', 0) + entry['Moving Time']
+                        existing['Cycle elapsed time'] = existing.get('Cycle elapsed time', 0) + entry['Elapsed Time']
+                    case 'Running' | 'Trail Running'::
+                        existing['Run distance'] = existing.get('Run distance', 0) + entry['Distance']
+                        existing['Run moving time'] = existing.get('Run moving time', 0) + entry['Moving Time']
+                        existing['Run elapsed time'] = existing.get('Run elapsed time', 0) + entry['Elapsed Time']
+                    case 'Walking':
+                        existing['Walk distance'] = existing.get('Walk distance', 0) + entry['Distance']
+                        existing['Walk moving time'] = existing.get('Walk moving time', 0) + entry['Moving Time']
+                        existing['Walk elapsed time'] = existing.get('Walk elapsed time', 0) + entry['Elapsed Time']
+                    case 'Open Water Swimming':
+                        existing['Swim distance'] = existing.get('Swim distance', 0) + entry['Distance']
+                        existing['Swim time'] = existing.get('Swim time', 0) + entry['Moving Time']
                         pass
-                case _:
-                    pass
-    # TODO: stringify everything for writing
+                    # case 'Other':
+                    #     if entry.get('Title', "").startswith("Elliptical Trainer"):
+                    #         pass
+                    case _:
+                        pass
+        return [by_date[date] for date in sorted(by_date.keys())]
+        # TODO: stringify everything for writing
+
+def combine_measurement_data(incoming_lists):
+    by_date = defaultdict(dict)
+    with BeginAndEndMessages("Combining measurement data"):
+        for list_in in incoming_lists:
+            for entry in list_in:
+                by_date[entry['Date']].update(entry)
+        return [by_date[date] for date in sorted(by_date.keys())]
+        # TODO: stringify everything for writing
 
 class PhysicalPanel(panels.DashboardPanel):
 
@@ -233,7 +271,8 @@ class PhysicalPanel(panels.DashboardPanel):
 
     def update(self, **kwargs):
 
-        """Merge incoming health-related data from various files, into one central file."""
+        """Merge incoming health-related data from various files, into two central files,
+        one for exercise and one for measurements."""
 
         self.exercise_data = dobishem.storage.combined(
             self.combined_exercise_filename,
@@ -245,7 +284,7 @@ class PhysicalPanel(panels.DashboardPanel):
             })
         self.measurement_data = dobishem.storage.combined(
             self.combined_measurement_filename,
-            combine_exercise_data,
+            combine_measurement_data,
             {
                 self.combined_measurement_filename: convert_measurement,
                 os.path.expandvars("$SYNCED/health/weight.csv"): convert_weight,
@@ -256,3 +295,12 @@ class PhysicalPanel(panels.DashboardPanel):
 
         self.updated = datetime.datetime.now()
         return self
+
+    def html(self):
+        return T.div(class_="physical")[expressionive.expridioms.wrap_box(
+            T.div(class_="measurements")[
+                T.p["There are %d measurement rows." % len(self.measurement_data)]
+            ],
+            T.div(class_="exercise")[
+                T.p["There are %d exercise rows." % len(self.exercise_data)]
+            ]
