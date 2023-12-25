596da6fee9d8be1d2944e4b0544424eadef29067
John C. G. Sturdy <jcg.sturdy@gmail.com>
2023-12-25 15:37:01 +0100

    Remove disused feature

--- qsutils/qschart.py
+++ qsutils/qschart.py
@@ -66,46 +66,6 @@ COLUMN_HEADERS = {'stone': 'St total',
 def column_header(column):
     return COLUMN_HEADERS.get(column, column)
 
-def munge_weights(data):
-    # data['Lbs total'] = pd.to_numeric(data['Stone']) * 14 + data['Lbs']
-    # data['St total'] = data['Lbs total'] / 14
-    # data['Kg'] = data['Lbs total'] * 0.453592
-    pass
-
-def munge_finances(data):
-    # data['Date'] = data['timestamp']
-    pass
-
-def munge_calories(data):
-    pass
-
-def munge_sleep(data):
-    data['Latency'] = data['onset_latency'] / 3600.0
-    for field in ['Duration',
-                  'Awake',
-                  'Light',
-                  'Rem',
-                  'Restless',
-                  'Deep',
-                  'Total']:
-        data[field] = data[field.lower()] / 3600.0
-
-MUNGERS = {
-    'weight': munge_weights,
-    'calories': munge_calories,
-    'finances': munge_finances,
-    'sleep': munge_sleep
-}
-
-def normalize_column_name(unit_name):
-    for k, v in POSSIBLE_COLUMN_NAMES.items():
-        if k.startswith(unit_name):
-            return v
-    return None
-
-def parsetime(timestr):
-    return datetime.datetime.strptime(timestr, "%Y-%m-%d") if isinstance(timestr, str) else timestr
-
 def qscharts(data:pd.DataFrame, file_type,
              timestamp,
              columns, begin, end, match, by_day_of_week,
@@ -182,11 +142,6 @@ def qschart(data: pd.DataFrame,
     if timestamp and os.path.exists(outfile) and os.path.getmtime(outfile) >= timestampe:
         return True
 
-    # do this before trimming to 'begin' and 'end', as this may create
-    # the date column they need:
-    if file_type in MUNGERS:
-        MUNGERS[file_type](data)
-
     if begin:
         data = data.loc[data['Date'] >= begin]
     if end:
@@ -233,41 +188,3 @@ def qschart(data: pd.DataFrame,
     fig.savefig(outfile,
                 facecolor=fig.get_facecolor())
     return True
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--debug", action='store_true')
-    parser.add_argument("-o", "--output", default="/tmp/weight.png")
-    parser.add_argument("-t", "--type")
-    parser.add_argument("-v", "--verbose", action='store_true')
-
-    parser.add_argument("-b", "--begin",
-                        help="""The earliest date to chart.""")
-    parser.add_argument("-e", "--end",
-                        help="""The latest date to chart.""")
-
-    parser.add_argument("-m", "--match", nargs=2,
-                        help="""The column to match on, and the regexp to match it against.""")
-
-    parser.add_argument("--column",
-                        action='append')
-
-    parser.add_argument("mainfile")
-    args = parser.parse_args()
-    if args.type is None:
-        file_type = file_types.deduce_file_type(args.mainfile)
-        if args.verbose:
-            print("Deduced file type", file_type)
-    else:
-        file_type = args.type
-
-    qschart(args.mainfile,
-            file_type,
-            [normalize_column_name(column) for column in args.column],
-            args.begin and parsetime(args.begin),
-            args.end and parsetime(args.end),
-            args.match,
-            args.output)
-
-if __name__ == "__main__":
-    main()
