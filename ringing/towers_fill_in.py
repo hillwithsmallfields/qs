dc57af573e353be5a1742ba8fbc970e05a9c7028
John Sturdy <jcg.sturdy@gmail.com>
2024-01-08 22:08:34 +0000

    store ringing analysis files in ringing directory

--- ringing/towers_fill_in.py
+++ ringing/towers_fill_in.py
@@ -14,21 +14,25 @@ def towers_fill_in_main():
 
     by_bells, by_weight, by_year = tower_visits.classify_towers(visits)
 
-    with open("/tmp/by-weight.csv", 'w') as bw_stream:
+    with open(os.path.expanduser("~/Sync/ringing/by-weight.csv"), 'w') as bw_stream:
         writer = csv.writer(bw_stream)
         writer.writerow(['Hundredweight', 'Towers'])
         for cwt in sorted(by_weight.keys(), reverse=True):
             writer.writerow([cwt, by_weight[cwt]])
-    with open("/tmp/by-bells.csv", 'w') as bb_stream:
+    with open(os.path.expanduser("~/Sync/ringing/by-bells.csv"), 'w') as bb_stream:
         writer = csv.writer(bb_stream)
         writer.writerow(['Bells', 'Towers'])
         for bells in sorted(by_bells.keys(), reverse=True):
             writer.writerow([bells, by_bells[bells]])
-    with open("/tmp/by-year.csv", 'w') as by_stream:
+    with open(os.path.expanduser("~/Sync/ringing/by-year.csv"), 'w') as by_stream:
         writer = csv.writer(by_stream)
         writer.writerow(['Year', 'Towers'])
         for year in sorted(by_year.keys(), reverse=True):
             writer.writerow([year, by_year[year]])
+        toured = [towers_that_year
+                  for year in by_year.keys()
+                  if (towers_that_year := by_year[year]) >= 20]
+        print("Average per touring year:", sum(toured) / len(toured))
 
 if __name__ == "__main__":
     towers_fill_in_main()
