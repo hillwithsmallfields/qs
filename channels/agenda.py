11e404cac93f72036050b9e42a31e9050408e557
John Sturdy <jcg.sturdy@gmail.com>
2026-02-11 20:44:10 +0000

    read agenda files directly

--- channels/agenda.py
+++ channels/agenda.py
@@ -1,25 +1,76 @@
 import datetime
-import json
 import os
-import subprocess
-import sys
+
+import orgparse
 
 from expressionive.expressionive import htmltags as T
 from expressionive.expridioms import wrap_box, labelled_subsection
 
 import channels.panels as panels
 
-def top_items(items):
-    return sorted(items, key=lambda item: item['position-in-file'])[:12]
+class AgendaItem:
 
-def org_ql_list(items):
-    # TODO: make this scrollable
-    return T.div(class_='agenda_list')[T.ul[[T.li[item['title']]
-                                             for item in items]]]
+    def __init__(self, heading, status, tags=None, properties=None, parent=None):
+        self.heading = heading
+        self.status = status
+        self.tags = tags or {}
+        self.properties = properties or {}
+        self.parent = parent
+        # caching:
+        self._timestamp = None
+
+    def __str__(self):
+        return f"{self.status} {self.heading}"
+
+    def last_state_change(self):
+        if not self._timestamp:
+            if 'last-state-change' in self.properties:
+                iso = self.properties['last-state-change']
+                self._timestamp = datetime.datetime.fromisoformat(iso[1:11] + "T" + iso[16:21])
+        return self._timestamp
+
+    def longname(self):
+        return self.heading + (""
+                               if self.parent is None
+                               else (" (in " + self.parent + ")"))
+
+def load_agenda_file(filename,
+                     n_results=None,
+                     require_todo=None,
+                     require_tag=None):
+    base_node = orgparse.load(os.path.expandvars(filename))
+    todo_keys = base_node.env.todo_keys
+    results = []
+
+    def add_entry_conditionally(entry):
+        if ((entry.todo == require_todo
+             if require_todo
+             else entry.todo in todo_keys)
+            and (require_tag is None
+                 or require_tag in entry.tags)):
+            results.append(AgendaItem(entry.get_heading(),
+                                      status=entry.todo,
+                                      tags=entry.tags,
+                                      properties=entry.properties,
+                                      parent=(None
+                                              if isinstance(entry.get_parent(), orgparse.node.OrgRootNode)
+                                              else entry.get_parent().get_heading())))
+    for top_entry in base_node.children:
+        if top_entry.children:
+            for sub_entry in top_entry.children:
+                add_entry_conditionally(sub_entry)
+                if n_results and len(results) == n_results:
+                    break
+        else:
+            add_entry_conditionally(top_entry)
+            if n_results and len(results) == n_results:
+                break
+    return results
 
 class AgendaPanel(panels.DashboardPanel):
 
     def __init__(self, *args, **kwargs):
+        print("in AgendaPanel.__init__")
         super().__init__(*args)
         self.input_files = set(("general.org", "shopping.org", "projects.org", "Marmalade-work.org"))
         self.from_org = None
@@ -34,44 +85,30 @@ class AgendaPanel(panels.DashboardPanel):
         return filenames & self.input_files
 
     def update(self, verbose=False, messager=None):
-
-        """Also updates the parcels expected list.
-        Files written:
-        * $SYNCED/var/views.json
-        * $SYNCED/var/parcels-expected.json"""
-        messager.print("running emacs subprocess for agenda queries")
-        result = subprocess.run(["emacs",
-                                 "--no-init-file",
-                                 "--batch",
-                                 "--script",
-                                 os.path.expandvars("$MY_ELISP/special-setups/dashboard/dashboard-emacs-query.el")],
-                                encoding='utf8',
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.STDOUT,
-                                text=True)
-        if verbose:
-            if result.stdout:
-                messager.print("emacs stdout:")
-                for line in result.stdout.split("\n"):
-                    messager.print("    "+ line)
-            else:
-                messager.print("Nothing on emacs stdout")
-        if result.returncode == 0:
-            self.from_org = self.storage.load(scratch='views.json')
-            if verbose:
-                messager.print(f"sections from org are {self.from_org.keys()}")
-            self.updated = datetime.datetime.now()
-        else:
-            messager.print("Emacs run for org query failed")
+        self.from_org = {
+            "General": load_agenda_file("$ORG/general.org", n_results=12),
+            "Weekend": load_agenda_file("$ORG/general.org", require_tag='weekend'),
+            "Projects": load_agenda_file("$ORG/projects.org", n_results=12),
+            "Marmalade": load_agenda_file("$SYNCED/vehicles/Marmalade/Marmalade-work.org", 6),
+            "Mending": load_agenda_file("$ORG/general.org", require_tag='mending'),
+            "Programming": load_agenda_file("$ORG/general.org", require_tag='programming'),
+            "Makespace": load_agenda_file("$ORG/projects.org", n_results=6, require_tag="@Makespace"),
+            "Ordered": load_agenda_file("$ORG/shopping.org", require_todo="ORDERED"),
+            "Dispatched": load_agenda_file("$ORG/shopping.org", require_todo="DISPATCHED"),
+            "Learning": load_agenda_file("$ORG/learning.org", require_todo="OPEN", n_results=6),
+            "Supermarket": load_agenda_file("$ORG/shopping.org", require_tag='supermarket', require_todo="BUY"),
+            "Mackays": load_agenda_file("$ORG/shopping.org", require_tag='Mackays', require_todo="BUY"),
+            "Project parts": load_agenda_file("$ORG/shopping.org", require_tag='project_parts', require_todo="BUY"),
+        }
         super().update(verbose, messager)
         return self
 
     def agenda_subsections(self, keys, messager=None):
         if self.from_org:
-            return wrap_box(*[labelled_subsection(key,
-                                                  org_ql_list(section_list))
-                              for key in keys
-                              if len(section_list := top_items(self.from_org.get(key, []))) > 0])
+            things = [labelled_subsection(key, T.ul[[T.li[item.longname()] for item in section_list]])
+                      for key in keys
+                      if len(section_list := self.from_org.get(key, [])) > 0]
+            return wrap_box(*things)
         else:
             messager.print("Warning: no agenda data")
             return T.p["No agenda data found"]
@@ -79,8 +116,8 @@ class AgendaPanel(panels.DashboardPanel):
     def html(self, messager=None):
         return wrap_box(
             labelled_subsection("Actions",
-                                self.agenda_subsections(["Today",
-                                                         "Imminent",
+                                self.agenda_subsections(["General",
+                                                         "Projects",
                                                          "Weekend",
                                                          "Mending",
                                                          "Marmalade,"
@@ -88,5 +125,4 @@ class AgendaPanel(panels.DashboardPanel):
                                                          "Programming"], messager)),
             labelled_subsection("Shopping",
                                 self.agenda_subsections(["Supermarket",
-                                                         "Mackays",
-                                                         "Online"], messager)))
+                                                         "Mackays"], messager)))
