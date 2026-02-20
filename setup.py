e7ce2952d18aed5b920809641e017e3a251c5c1a
John Sturdy <jcg.sturdy@gmail.com>
2026-02-20 11:57:17 +0000

    add project files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="qs",
+    version="0.0.11",
+    description="My Quantified self and other updates.",
+    author="John C. G. Sturdy",
+    author_email="jcg.sturdy@gmail.com",
+    packages=find_packages(),
+)
